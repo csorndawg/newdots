854a366c19e8cb84650156a42fc5ba6b6fd3427e
csorndawg <zaccsorny@yahoo.com>
2024-07-22 23:19:41 +0000

    first

--- utils.d/make_symlinks.sh
+++ utils.d/make_symlinks.sh
@@ -3,20 +3,23 @@
 # Basic script for generating dotfile symlinks 
 
 DOTFILES_HOME="$HOME/.dotfiles"
-ln -sr ~/GIT/newdots $DOTFILES_HOME
-ln -sr ~/GIT/newdots $HOME/dotfiles
+ln -sr ~/GIT/newdots $DOTFILES_HOME #2> /dev/null
+ln -sr ~/GIT/newdots $HOME/dotfiles #2> /dev/null
 
 # GIT
-ln -srf $DOTFILES_HOME/gitd/gitignore.minimal $HOME/.gitignore
-ln -srf $DOTFILES_HOME/gitd/gitconfig.minimal $HOME/.gitconfig
+ln -sr $DOTFILES_HOME/gitd/gitignore.minimal $HOME/.gitignore #2> /dev/null
+ln -sr $DOTFILES_HOME/gitd/gitconfig.minimal $HOME/.gitconfig #2> /dev/null
 
 # BASH/ZSH
 if [[ "$SHELL" = "/bin/zsh" ]];
 then
     #TODO: add "zshd"
-    export zshrc="$DOTFILES_HOME/zshd/zshrc"
+    #export ZSHRC="$DOTFILES_HOME/zshd/zshrc"
+    echo "Zsh shell"
 else
-    ln -srf "$DOTFILES_HOME/bashd/bashrc" "$HOME/.bashrc"
+    echo "Bash shell"
+    ln -sr "$DOTFILES_HOME/bashd/bashrc" "$HOME/.bashrc" 2> /dev/null
+    #export BASHRC="$DOTFILES_HOME/bashd/bashrc"
 fi
 
 
@@ -24,25 +27,25 @@ fi
 if [[ -f "$HOME/.local/bin/lvim" ]];
 then 
     echo "Sourcing lunarvim"
-    ln -srf "$DOTFILES_HOME/nvimd/lvimd" "$XDG_CONFIG_HOME/lvim"
-    EDITOR=lvim
-    export LVIM_CONFIG="$XDG_CONFIG_HOME/lvim"
+    ln -sr "$DOTFILES_HOME/nvimd/lvimd" "$XDG_CONFIG_HOME/lvim" #2> /dev/null
+    #export EDITOR=lvim
+    #export LVIM_CONFIG="$XDG_CONFIG_HOME/lvim"
 
 elif [[ -f "/usr/bin/nvim" ]];
 then
     echo "Sourcing neovim"
-    ln -srf "$DOTFILES_HOME/nvimd" "$XDG_CONFIG_HOME/nvim"
-    EDITOR=nvim
-    export NVIM_CONFIG="$XDG_CONFIG_HOME/nvim"
+    ln -sr "$DOTFILES_HOME/nvimd" "$XDG_CONFIG_HOME/nvim" #2> /dev/null
+    #export EDITOR=nvim
+    #export NVIM_CONFIG="$XDG_CONFIG_HOME/nvim"
 
 else
     echo "Source vim"
-    ln -srf "$DOTFILES_HOME/vimd" "$HOME/.vim"
-    ln -srf "$DOTFILES_HOME/vimd/vimrc" "$HOME/.vimrc"
-    EDITOR=vim 
+    ln -sr "$DOTFILES_HOME/vimd" "$HOME/.vim" #2> /dev/null
+    ln -sr "$DOTFILES_HOME/vimd/vimrc" "$HOME/.vimrc" #2> /dev/null
+    export EDITOR=vim 
 fi
 
 
 # TMUX 
-ln -srf $DOTFILES_HOME/tmuxd $HOME/.config/tmux
-ln -srf $DOTFILES_HOME/tmuxd/tmux.conf $HOME/.tmux.conf
+ln -sr $DOTFILES_HOME/tmuxd $HOME/.config/tmux #2> /dev/null
+ln -sr $DOTFILES_HOME/tmuxd/tmux.conf $HOME/.tmux.conf #2> /dev/null

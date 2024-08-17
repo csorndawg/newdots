ab46e9deedc46365102e6d8aa54ceb89191411bc
csorndawg <zaccsorny@yahoo.com>
2024-08-17 03:03:48 +0000

    Add preferences.lua file to configure nvim options. Add code to init.lua to source preferences

--- nvim.d/init.lua
+++ nvim.d/init.lua
@@ -1,2 +1,6 @@
+-- source lua submods
+require("preferences")
+
+-- update colorscheme
 -- vim.cmd("colorscheme nordic")  -- throws error, commenting out for now
 vim.cmd("colorscheme slate")
---
+++ nvim.d/lua/preferences.lua
@@ -0,0 +1,7 @@
+-- extra whitespace around "=" not permitted
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set showmatch")
+--vim.cmd("set wildmenu=true")
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")

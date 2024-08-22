eacc777b142cb413205f77c6f6b479b1adaec8e0
csorndawg <zaccsorny@yahoo.com>
2024-08-22 23:09:04 +0000

    Ongoing configuration tuning

---
+++ zsh.d/custom/lib/functions.zsh
@@ -0,0 +1,12 @@
+#
+# Custom zsh-only functions
+#
+
+
+# jump to directory from anywhere via z and create and cd into new subdir 
+#zmkd(){
+#    Z_PATTERN="$1"
+#    NEW_DIR="$2"
+#    z "$Z_PATTERN"
+#    mkdir -p "$NEWDIR" && cd "$NEWDIR"
+#}

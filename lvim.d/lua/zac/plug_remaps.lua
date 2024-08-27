489ca5f891cc9091438e2203c0102e557750e5cf
csorndawg <zaccsorny@yahoo.com>
2024-08-27 19:56:15 +0000

    Remove lunarvim

--- lvim.d/lua/zac/plug_remaps.lua
+++
@@ -1,9 +0,0 @@
-
--- todo-comments
-vim.keymap.set("n", "<leader>tdn", function()
-  require("todo-comments").jump_next({keywords = { "ERROR", "WARNING", "TODO" }})
-end, { desc = "Next todo comment" })
-
-vim.keymap.set("n", "<leader>tdp", function()
-  require("todo-comments").jump_prev()
-end, { desc = "Previous todo comment" })

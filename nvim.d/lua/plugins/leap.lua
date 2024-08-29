5a267b7b2b0b6a0df206a0dcca8b584872ce9d7a
csorndawg <zaccsorny@yahoo.com>
2024-08-29 16:27:32 -0400

    Add experimental/wip changes

--- nvim.d/lua/plugins/leap.lua
+++ nvim.d/lua/plugins/leap.lua
@@ -1,9 +1,20 @@
 return {
     {
       "ggandor/leap.nvim",
-      name = "leap",
-      config = function()
-        require("leap").add_default_mappings()
+      enabled = true,
+      keys = {
+          { "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
+          { "S", mode = { "n", "x", "o" }, desc = "Leap Backwards to" },
+          { "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
+      },
+      config = function(_, opts)
+        local leap = require("leap")
+        for k, v in pairs(opts) do
+          leap.opts[k] = v
+        end
+	    leap.add_default_mappings(true)
+	    vim.keymap.del({ "x", "o" }, "x")
+	    vim.keymap.del({ "x", "o" }, "X")
       end,
-    },
+    }
 }
---
+++ nvim.d/lua/snippets/sql.lua
@@ -0,0 +1,4 @@
+--return {
+	--s("trig", t("loaded!!"))
+--}
+

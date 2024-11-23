4d0fd3643881167e6e2fbe98bff7f442699a2016
csorndawg <zaccsorny@yahoo.com>
2024-11-23 09:03:34 +0000

    Add nvim-lsp plugin - install works, configuration still ongoing

---
+++ nvim.d/init.lua
@@ -0,0 +1,44 @@
+--- ---------------------------------------------------
+---   bootstrap lazy.nvim
+-- ---------------------------------------------------
+--- lazy config install
+--- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- must set leader/local leader before loading lazy
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- lazy.nvim setup
+require("lazy").setup({
+  spec = {
+    -- import all plugins (plus their configurations) defined
+    -- in nvim/lua/plugins.lua
+    { import = "plugins" },
+  },
+})
+
+-- ---------------------------------------------------
+--  source custom nvim config submodules
+-- ---------------------------------------------------
+
+--require("config.opts")
+--require("config.keymaps")
+--require("config.autocmds")
+
+-- load colorscheme from nvim.d/lua/extra/colorscheme.lua
+require("extra.colorscheme")
--- nvim.d/lua/plugins/nvim-lspconfig.lua
+++ nvim.d/lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,5 @@
 return {
   {
-    neovim/nvim-lspconfig,
+    "neovim/nvim-lspconfig",
   }
 }

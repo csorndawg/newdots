a891b412a6af9f2d39f1334fcc1ff55904f2eade
csorndawg <zaccsorny@yahoo.com>
2024-11-24 03:11:00 -0500

    Add first commit - dependency scripts are still WIPs.

---
+++ nvim.d/dependencies/nvim_lsp_server_system_installs.sh
@@ -0,0 +1,15 @@
+# NEOVIM LSP SERVER SYSTEM INSTALLS
+
+## @TODO - move section from here to neovim from scratch installer script
+## OS/SYSTEM PREREQS INSTALLS
+npm install --global yarn 	# needed for YAML LSP server
+
+## INSTALL LSP SERVER(S) ON SYSTEM 
+## NOTE - below doesn't cover all LSP servers that I work with, and more work/diff LSP servers will be needed. All extra work should be added to this file.
+sudo npm i -g bash-language-server 
+sudo npm i -g pyright
+sudo npm i -g vim-language-server
+sudo npm i -g vscode-langservers-extracted  #json
+sudo yarn global add yaml-language-server #yaml
+
+

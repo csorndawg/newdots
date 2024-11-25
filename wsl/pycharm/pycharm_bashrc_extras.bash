c4879b81e48247e9e39540a18968c2a18d1959bd
csorndawg <zaccsorny@yahoo.com>
2024-11-24 19:48:45 -0500

    Add new WSL dotfile directory for sourcing WSL/Local Asus configs in bashrc. Update bashrc to source refactored WSL and prompt logic

---
+++ wsl/pycharm/pycharm_bashrc_extras.bash
@@ -0,0 +1,10 @@
+# WSL-only configurations
+
+alias charm="/mnt/c/Users/zaccs/AppData/Local/JetBrains/Toolbox/apps/PyCharm-P/ch-0/<version>/bin/pycharm64.exe"
+alias pycharm_homedir="cd /mnt/c/Users/zaccs/PycharmProjects/"
+alias pychh="cd /mnt/c/Users/zaccs/PycharmProjects/"
+alias vo_pycharm="source  /mnt/c/Users/zaccs/PycharmProjects/PythonProject2/.venv/Scripts/activate"
+export PYCHARM_HOME_DIR="/mnt/c/Users/zaccs/PycharmProjects"
+export PyCharmPath="/mnt/c/Users/zaccs/PycharmProjects/"
+export VENV_PYCHARM="/mnt/c/Users/zaccs/PycharmProjects/PythonProject2"
+export XDG_PYCHARM_HOME="/mnt/c/Users/zaccs/PycharmProjects/"
---
+++ wsl/todo_wsl.md
@@ -0,0 +1,8 @@
+
+# WSL cleanup/todo tasks
+
+- Fix FZF ***
+- remove annoying warning/non failure errors from bashrc sourcing like curses gem
+- refactor remaining WSL only logic still in bashrc_wsl 
+
+

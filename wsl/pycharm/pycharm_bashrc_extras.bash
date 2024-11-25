b39636c23435a5d3c2f514d0c99ab33afc336f71
csorndawg <zaccsorny@yahoo.com>
2024-11-24 19:48:45 -0500

    Add new WSL dotfile directory for sourcing WSL/Local Asus configs in bashrc

---
+++ wsl/pycharm/pycharm_bashrc_extras.bash
@@ -0,0 +1,4 @@
+export VENV_PYCHARM="/mnt/c/Users/zaccs/PycharmProjects/PythonProject2"
+export PYCHARM_HOME_DIR="/mnt/c/Users/zaccs/PycharmProjects"
+alias vo_pycharm="source  /mnt/c/Users/zaccs/PycharmProjects/PythonProject2/.venv/Scripts/activate"
+

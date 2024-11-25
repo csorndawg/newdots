3b75bb5fd64ac852c32055c1e90cf06a53b33251
csorndawg <zaccsorny@yahoo.com>
2024-11-24 23:37:33 -0500

    Add fzf-bash post install/setup script, clean up some bashrc code.

--- utils.d/fzf.bash
+++ utils.d/fzf.bash
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/zc/.vim/plugged/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/zc/.vim/plugged/fzf/bin"
+if [[ ! "$PATH" == */home/zc/.local/work/git/newdots/vim.d/plugged/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/zc/.local/work/git/newdots/vim.d/plugged/fzf/bin"
 fi
 
 eval "$(fzf --bash)"

e6ad9d18f8580e633e5f9f9cd4c1ea538a62da1c
csorndawg <zaccsorny@yahoo.com>
2024-11-24 20:36:59 -0500

    Add scripts to handle pre/post install dependency issues for git and nvim dotfiles.

--- git.d/git_dependencies.sh
+++ git.d/git_dependencies.sh
@@ -10,8 +10,21 @@ GIT_DOTFILES_HOME="$HOME/dotfiles/git.d"
 
 # exit if dotfile bashrc symlink is active
 # SYMLINK_FILES=(gitignore gitconfig)
-##SYMLINK_CHECK=$(ls -la $HOME | egrep "\.git(ignore|config)\b" | egrep -c "^l")
-#
+
+# @FIXME - logic needs to be fixed 
+for git_symlink in $(find $GIT_DOTFILES_HOME -type f); do
+	for SYML_PATTERN in ${SYMLINK_FILES[@]}; do
+		isMatch=$(echo $git_symlink | egrep -cv "$SYML_PATTERN" | bc)
+		if [ $isMatch -ge 1 ]; then
+			echo "Match found for $SYML_PATTERN with $git_symlink"
+			echo "Making symlink b/w above 2"
+			break
+		fi
+	done
+done
+
+
+
 # <PERFORM SYMLINK_CHECK for each `file` in $SYMLINK_FILES and make symlink if DNE>		# <<<-------- @TODO
 #
 #echo "SYMLINK_CHECK: $SYMLINK_CHECK"

50718fb100e2b0ac2d421ea9a3f70ee94f13bf74
csorndawg <zaccsorny@yahoo.com>
2024-11-27 00:53:28 +0000

    Add lib/expr folder for experimental bash configs. Add lib/expr sourcing function to bashrc

---
+++ bash.d/lib/expr/aliases.bash
@@ -0,0 +1,6 @@
+# EXPERIMENTAL BASHRC ALIASES
+alias numFiles='_omb_util_print $(ls -1 | wc -l)'       # numFiles:     Count of non-hidden files in current dir
+alias make1mb='truncate -s 1m ./1MB.dat'     # make1mb:      Creates a file of 1mb size (all zeros)
+alias make5mb='truncate -s 5m ./5MB.dat'     # make5mb:      Creates a file of 5mb size (all zeros)
+alias make10mb='truncate -s 10m ./10MB.dat'  # make10mb:     Creates a file of 10mb size (all zeros)
+

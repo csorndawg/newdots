f23b445813ccd697c2c45f2f5cc2a25e26795627
csorndawg <zaccsorny@yahoo.com>
2024-11-27 23:24:46 +0000

    Update code to backup existing bashrc file before overwritting

--- bash.d/bashrc_dependencies.sh
+++ bash.d/bashrc_dependencies.sh
@@ -2,13 +2,24 @@
 
 BASH_DOTFILES_HOME="$HOME/dotfiles/bash.d"
 
-# exit if dotfile bashrc symlink is active
-SYMLINK_CHECK=$(ls -la $HOME | egrep "\.bashrc\b" | egrep -c "^l")
+# conditional vars
+SYMLINK_CHECK=$(ls -la $HOME | egrep "\.bashrc\b" | egrep -c '^l')
+FILE_CHECK=$(ls -la $HOME | egrep "\.bashrc\b" | egrep -c '^-')
 echo "SYMLINK_CHECK: $SYMLINK_CHECK"
-if [ $SYMLINK_CHECK -ge 1 ]; then
-	echo "Current bashrc file is already symlinked to dotfiles bashrc"
-	exit 0
-fi
+echo "FILE_CHECK: $FILE_CHECK"
 
-# make symlink 
-ln -sr "$BASH_DOTFILES_HOME/bashrc_wsl" "$HOME/.bashrc" 2> /dev/null
+# dont overwrite existing bashrc symlink
+if [ $SYMLINK_CHECK -ge 1 ]; then
+	echo "bashrc symlink exists and will not be overwritten."
+	exit 0
+
+# if file (not symlink) exists backup before overwritting
+elif [ $FILE_CHECK -ge 1 ]; then
+	echo "Backing up current bashrc file before generating dotfile bashrc symlink"
+	# back up existing bashrc file
+	mv -vf "$HOME"/.bashrc{,.bk}
+	ln -sr "$BASH_DOTFILES_HOME/bashrc_wsl" "$HOME/.bashrc" 2> /dev/null
+## this should never execute
+else
+	exit 1
+fi

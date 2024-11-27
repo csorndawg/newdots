c79760bc15ce0360ccf1450b639ebd7091bd2425
csorndawg <zaccsorny@yahoo.com>
2024-11-27 05:57:29 +0000

    Add aliases.bash file. Moved handful of aliases from bashrc to dedicated alias file - more still need to be moved. Renamed .bashrc extension to .bash and fixed code in bashrc to reflect new extension.

---
+++ bash.d/aliases.bash
@@ -0,0 +1,22 @@
+# WIP 
+# ACTIVE/STABLE BASH ALIASES
+# UNTESTED/EXPERIMENTAL ALIASES CAN BE FOUND IN lib/expr/aliases.bash
+
+##
+## @TODO
+## ORGANIZE ALIASES INTO GROUPS/SECTIONS HERE AFTER MIGRATING 
+## FROM BASHRC_WSL or FROM lib/expr/aliases.bash AFTER BEING TESTED
+##
+
+
+##
+## BASHRC_WSL MIGRATED ALIASES
+##
+alias gmnf="git merge --no-ff"
+alias gm="git merge"
+alias gpsu="git push --set-upstream origin"
+alias gbd="git branch --delete"
+alias nv="nvim"
+alias v="vim"
+alias vi="vim"
+

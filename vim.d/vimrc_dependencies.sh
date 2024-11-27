32b7b7a247f0e4525f3399dcd32b660d27cc079d
csorndawg <zaccsorny@yahoo.com>
2024-11-27 04:11:04 +0000

    Fix code for creating symlink

--- vim.d/vimrc_dependencies.sh
+++ vim.d/vimrc_dependencies.sh
@@ -7,15 +7,19 @@ VIM_SYMLINK_PATTERNS=(vim vimrc)
 # make dirs dependencies
 
 for x in ${VIM_SYMLINK_PATTERNS[@]}; do
-	echo "x: $x"
+	#echo "x: $x"
 	SYMLINK_CHECK=$(ls -la $HOME | egrep "\.$x\b" | egrep -c "^l")
 	if [ $SYMLINK_CHECK -ge 1 ]; then
 		echo "Current symlink target $x already exists, and will be skipped."
 		continue
 	fi
 	
-	# otherwise symlink needs to be created
-	ln -
+	case "$x" in
+		vimrc)
+			ln -sr "$HOME/dotfiles/vim.d/vimrc" "$HOME/.vimrc"
+			;;
+		**)
+			ln -sr "$HOME/dotfiles/vim.d" "$HOME/.vim"
+			;;
+	esac
 done
-
-

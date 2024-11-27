60fe758f221446dbcddda98d183cffe39c2a6f5e
csorndawg <zaccsorny@yahoo.com>
2024-11-27 21:11:34 +0000

    Clean up and fix symlinking logic for git dotfiles

--- git.d/git_dependencies.sh
+++ git.d/git_dependencies.sh
@@ -8,30 +8,18 @@
 
 GIT_DOTFILES_HOME="$HOME/dotfiles/git.d"
 
-# exit if dotfile bashrc symlink is active
-# SYMLINK_FILES=(gitignore gitconfig)
-
-# @FIXME - logic needs to be fixed 
-for git_symlink in $(find $GIT_DOTFILES_HOME -type f); do
-	for SYML_PATTERN in ${SYMLINK_FILES[@]}; do
-		isMatch=$(echo $git_symlink | egrep -cv "$SYML_PATTERN" | bc)
-		if [ $isMatch -ge 1 ]; then
-			echo "Match found for $SYML_PATTERN with $git_symlink"
-			echo "Making symlink b/w above 2"
-			break
-		fi
-	done
+for gf in $(find $GIT_DOTFILES_HOME -type f); do
+	BASENAME="$(basename $gf)"
+	case $BASENAME in 
+		gitignore)
+			echo "gitignore"
+			echo ln -sr "$GIT_DOTFILES_HOME/$BASENAME" "$HOME/.$BASENAME"
+			ln -sr "$GIT_DOTFILES_HOME/$BASENAME" "$HOME/.$BASENAME"
+			;;
+		gitconfig)
+			echo "gitconfig"
+			echo ln -sr "$GIT_DOTFILES_HOME/$BASENAME" "$HOME/.$BASENAME"
+			ln -sr "$GIT_DOTFILES_HOME/$BASENAME" "$HOME/.$BASENAME"
+			;;
+	esac
 done
-
-
-
-# <PERFORM SYMLINK_CHECK for each `file` in $SYMLINK_FILES and make symlink if DNE>		# <<<-------- @TODO
-#
-#echo "SYMLINK_CHECK: $SYMLINK_CHECK"
-#if [ $SYMLINK_CHECK -ge 1 ]; then
-#	echo "Current bashrc file is already symlinked to dotfiles bashrc"
-#	exit 0
-#fi
-
-# make symlink 
-#ln -sr "$BASH_DOTFILES_HOME/bashrc_wsl" "$HOME/.bashrc" 2> /dev/null

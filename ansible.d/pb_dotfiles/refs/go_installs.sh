d965489b851020791abab6da9f7b733860c0bea2
csorndawg <zaccsorny@yahoo.com>
2024-11-28 18:50:11 +0000

    Add local ansible directory to env. exports. Add ansible.d directory. Add playbook that installs handful of apt packages, mysql, and mysql db file to series of hosts

---
+++ ansible.d/pb_dotfiles/refs/go_installs.sh
@@ -0,0 +1,5 @@
+# Dev Env. GO Dependencies
+
+## cheat 
+go install github.com/cheat/cheat/cmd/cheat@latest
+# export CHEAT_CONFIG_PATH="~/dotfiles/config.d/cheat/conf.yml"
---
+++ ansible.d/pb_dotfiles/refs/npm_installs.sh
@@ -0,0 +1,21 @@
+# List of `npm` installs to convert into ansible for dotfiles/dev env.
+
+
+## Neovim
+
+### Plugin Dependencies
+
+
+### LSP
+npm install --global yarn
+sudo npm i -g bash-language-server
+sudo npm i -g pyright
+sudo npm i -g vim-language-server
+sudo npm i -g vscode-langservers-extracted  #json
+sudo yarn global add yaml-language-server #yaml
+
+
+## Oneoffs
+npm install -g tldr
+
+

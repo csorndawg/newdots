7932a683ada8020540bf3c88683935355c427387
csorndawg <zaccsorny@yahoo.com>
2024-11-29 19:12:29 +0000

    Still work in progress. Add playbook that downloads neovim's apt-dependencies.

---
+++ ansible.d/pb_dotfiles/refs/apt_installs.sh
@@ -0,0 +1,23 @@
+# Dotfiles `apt` Install Dependencies 
+
+## apt only
+sudo apt install rlwrap
+sudo apt install ripgrep
+sudo apt install make
+sudo apt install python3-pip
+sudo apt install nodejs
+sudo apt install npm
+sudo apt install unzip
+sudo apt install fzf
+
+## apt with post-install conf
+
+### bat
+sudo apt install bat
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
+### fd
+sudo apt install fd-find
+mkdir -p ~/.local/bin
+ln -s $(which fdfind) ~/.local/bin/fd

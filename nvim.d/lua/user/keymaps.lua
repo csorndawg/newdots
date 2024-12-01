5ab66a52aa58de3d0f126929070eb6a866f05c32
csorndawg <zaccsorny@yahoo.com>
2024-12-01 03:36:32 +0000

    Ongoing keymaps refining

--- nvim.d/lua/user/keymaps.lua
+++ nvim.d/lua/user/keymaps.lua
@@ -10,25 +10,21 @@
 --   term_mode = "t",
 --   command_mode = "c",
 
-
--- Lua Locals 
-local opts = { noremap = true, silent = true}
-local opts_loud = { noremap = true, silent = false}
+-- Lua Locals
+local opts = { noremap = true, silent = true }
+local opts_loud = { noremap = true, silent = false }
 local term_opts = { silent = true }
-local remapp = vim.api.nvim_set_keymap	-- shorten NVIM remap function call
-
+local remapp = vim.api.nvim_set_keymap -- shorten NVIM remap function call
 
 --
--- Normal Mode 
+-- Normal Mode
 --
-
 
 -- better window navigation
 remapp("n", "<C-h>", "<C-w>h", opts)
 remapp("n", "<C-j>", "<C-w>j", opts)
 remapp("n", "<C-k>", "<C-w>k", opts)
 remapp("n", "<C-l>", "<C-w>l", opts)
-
 
 -- resize with arrows
 remapp("n", "<C-Up>", ":resize -2<CR>", opts)
@@ -47,78 +43,72 @@ remapp("n", "<A-j>", ":m .+1<CR>==", opts)
 remapp("n", "<A-k>", ":m .-2<CR>==", opts)
 
 -- Normal/Command mode switch 	(Cycle b/w two modes with `;;`)
-remapp("n", "<Leader>;", ":", {silent = false} ) --  dont pass "opts" b/c we want to see CMD prompt
-remapp("n", "<Leader>;;", ":", {silent = false} ) --  dont pass "opts" b/c we want to see CMD prompt
+remapp("n", "<Leader>;", ":", { silent = false }) --  dont pass "opts" b/c we want to see CMD prompt
+remapp("n", "<Leader>;;", ":", { silent = false }) --  dont pass "opts" b/c we want to see CMD prompt
 --remapp("n", ";;", ":", {silent = false} ) -- opts)  -- @TODO: Find suitable alternate, since this interfers with '[fF]/[tT]' cycling
 
 -- Normal/Insert mode switch 	(Cycle b/w two modes with `,,`)
 remapp("n", "<Leader>,", "i", opts)
-remapp("n", "<Leader>jj", "i", opts)  -- @experimental
-remapp("n", ",,", "i", opts)  -- @experimental
+remapp("n", "<Leader>jj", "i", opts) -- @experimental
+remapp("n", ",,", "i", opts) -- @experimental
 
 -- toggle search highlighting
-remapp('n', '<Esc>', '<cmd>nohlsearch<CR>', opts)
-
+remapp("n", "<Esc>", "<cmd>nohlsearch<CR>", opts)
 
 -- @experimental
 -- quickfix navigation
-remapp('n', '<leader>qo', 'copen', {silent = false})
-remapp('n', '<leader>qc', 'cclose', {silent = false})
-remapp('n', '<leader>qn', 'cnext', {silent = false})
-remapp('n', '<leader>qp', 'cprev', {silent = false})
-
+remapp("n", "<leader>qo", "copen", { silent = false })
+remapp("n", "<leader>qc", "cclose", { silent = false })
+remapp("n", "<leader>qn", "cnext", { silent = false })
+remapp("n", "<leader>qp", "cprev", { silent = false })
 
 -- easier saving/exiting
-remapp('n', '<leader>w', ':w<Cr>', opts)
-remapp('n', '<leader>leader>w', ':w!<Cr>', opts)
-remapp('n', '<leader>x', ':q<Cr>', opts)
-remapp('n', '<leader><leader>x', ':q!<Cr>', opts)  -- DONT overwrite 'qq' (start macro recording)
+remapp("n", "<leader>w", ":w<Cr>", opts)
+remapp("n", "<leader>leader>w", ":w!<Cr>", opts)
+remapp("n", "<leader>x", ":q<Cr>", opts)
+remapp("n", "<leader><leader>x", ":q!<Cr>", opts) -- DONT overwrite 'qq' (start macro recording)
 
 -- if possible, create new vert window from current file
-vim.keymap.set('n', '<C-n>', function()
-  local current_file = vim.fn.expand('%:t')  -- get current file name
-  if current_file == '' then
-    vim.cmd('vnew')  -- create vert split w/ new unnamed buffer
-  else
-    vim.cmd('vnew ' .. current_file)  -- create vert split w/ current file name
-  end
+vim.keymap.set("n", "<C-n>", function()
+	local current_file = vim.fn.expand("%:t") -- get current file name
+	if current_file == "" then
+		vim.cmd("vnew") -- create vert split w/ new unnamed buffer
+	else
+		vim.cmd("vnew " .. current_file) -- create vert split w/ current file name
+	end
 end, { noremap = true, silent = true })
 
-
 -- @kickstarter
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Normal/Terminal
-remapp('n', '<Leader>t', ':terminal<Cr>iclear<CR>', {noremap = true })
+remapp("n", "<Leader>t", ":terminal<Cr>iclear<CR>", { noremap = true })
 
 -- Normal/Visual
-remapp("n", "<leader>v","v", {noremap = true})
+remapp("n", "<leader>v", "v", { noremap = true })
 
 -- Normal/Visual Line
-remapp("n", "<leader><leader>v","<S-v>", {noremap = true})
-
+remapp("n", "<leader><leader>v", "<S-v>", { noremap = true })
 
 -- Cursor Movement within current screen
-remapp("n", "<leader><leader>h", "H", {silent = true} )
-remapp("n", "<leader><leader>m", "M", {silent = true} )
-remapp("n", "<leader><leader>l:", "L", {silent = true} )
+remapp("n", "<leader><leader>h", "H", { silent = true })
+remapp("n", "<leader><leader>m", "M", { silent = true })
+remapp("n", "<leader><leader>l:", "L", { silent = true })
 
--- undo/redo 
+-- undo/redo
 remapp("n", "<leader>u", "<C-u>", opts)
-remapp('n', '<leader>r', '<C-r>', {noremap = true})
-
+remapp("n", "<leader>r", "<C-r>", { noremap = true })
 
 -- show path of current file
-remapp("n", "<Leader>ep", ":echo expand('%')<Cr>", {noremap = true})
-
+remapp("n", "<Leader>ep", ":echo expand('%')<Cr>", { noremap = true })
 
 -- add description later
 -- run (p)ython/(b)ash buffer
-remapp("n", "<leader>rp", ":w !python3 %<Tab><Cr>", {noremap = true} )
+remapp("n", "<leader>rp", ":w !python3 %<Tab><Cr>", { noremap = true })
 remapp("n", "<leader>rb", ":w !bash  %<Tab><Cr>", opts)
 
 --
--- Insert Mode 
+-- Insert Mode
 --
 
 -- Insert/Normal mode swtich
@@ -130,9 +120,8 @@ remapp("i", "jj", "<ESC>", opts)
 remapp("i", "kk", "<ESC>", opts)
 
 -- Insert/Command mode switch
-remapp("i",";;", "<ESC>:", opts_loud) -- dont pass opts b/c we want to see CMD prompt
-remapp("i","<Alt-;>", "<ESC>:", opts_loud)
-
+remapp("i", ";;", "<ESC>:", opts_loud) -- dont pass opts b/c we want to see CMD prompt
+remapp("i", "<Alt-;>", "<ESC>:", opts_loud)
 
 --
 -- Visual Mode
@@ -149,14 +138,21 @@ remapp("v", "p", '"_dP', opts)
 
 -- mirror tmux Visual/Copy mode keybinds
 -- switch to normal mode/kill current selection
-remapp("v", "<leader>.","<C-c>", {noremap = true})
-remapp("v", "..","<C-c>", {noremap = true})
---remapp("v", ",,","<C-c>", {noremap = true})
+remapp("v", "<leader>.", "<C-c>", { noremap = true })
 
+-- Tmux-like remap for entering/exiting visual block/copy mode
+remapp("n", "<C-.>", "<C-v>", { noremap = true })
+remapp("v", "<C-.>", "<C-c>", { noremap = true })
+remapp("v", "<C-/>", "y", { noremap = true })
+--remapp("v", ",,","<C-c>", {noremap = true})
 
 -- @experimental
 -- visual mode copy alt.
-remapp("v", "//", "y", {noremap = true})
+remapp("v", "//", "y", { noremap = true })
+
+-- Selection Mode Only
+-- 'x' deletes selection
+remapp("v", "x", "d", { noremap = true })
 
 --
 -- Visual Block Mode
@@ -168,21 +164,21 @@ remapp("x", "K", ":m '<-2<CR>gv=gv", opts)
 remapp("x", "<A-j>", ":m '>+1<CR>gv=gv", opts)
 remapp("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
 
-
 --
 -- Terminal Mode
 --
 
 -- better terminal navigation
--- remapp("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+--- remapp("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 -- remapp("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- remapp("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- remapp("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
--- @TODO 
+-- @TODO
 -- Find way to simluate another <CR> after Neovim ends the terminal process.
 -- Tried adding second <CR> but it didnt work (guessing its b/c of some Operator pending thing).
-vim.keymap.set('t', '<C-x>', '<C-c> exit<CR>', { desc = 'Exit terminal mode' })
+vim.keymap.set("t", "<C-x>", "<C-c> exit<CR>", { desc = "Exit terminal mode" })
+-- remapp("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 --
 -- Command Mode
@@ -192,28 +188,26 @@ vim.keymap.set('t', '<C-x>', '<C-c> exit<CR>', { desc = 'Exit terminal mode' })
 remapp("c", ";;", "<Esc>", opts)
 remapp("c", "<A-;>", "<Esc>", opts)
 
--- command/insert mode switch  (continue with comma `,` theme remaps linked with Insert Mode) 
+-- command/insert mode switch  (continue with comma `,` theme remaps linked with Insert Mode)
 remapp("c", "<A-,>", "<Esc>i", opts)
 remapp("c", ",,", "<Esc>i", opts)
 --remapp("c", "<A-;;>", "<Esc>", opts)
-
-
 
 -- @TODO - Try out <CTRL><key> vs. <key><key> movement remaps, and pick one method
 -- to keep moving forward and remove other method after to free up binding.
 -- CMD mode command line cursor movement remaps
 
 -- <key><key> CMD movement remaps
-remapp("c", "HH", "<Home>", {noremap = true})
-remapp("c", "EE", "<End>", {noremap = true})
-remapp("c", "bb", "<C-Left>", {noremap = true})
-remapp("c", "ww", "<C-Right>", {noremap = true})
-remapp("c", "hh", "<Left>", {noremap = true})
-remapp("c", "ll", "<Right>", {noremap = true})
+remapp("c", "HH", "<Home>", { noremap = true })
+remapp("c", "EE", "<End>", { noremap = true })
+remapp("c", "bb", "<C-Left>", { noremap = true })
+remapp("c", "ww", "<C-Right>", { noremap = true })
+remapp("c", "hh", "<Left>", { noremap = true })
+remapp("c", "ll", "<Right>", { noremap = true })
 
 -- CMD history cycling remaps
-remapp("c", "<C-,>", "<Up>", {noremap = true})
-remapp("c", "<C-.>", "<Down>", {noremap = true})
+remapp("c", "<C-,>", "<Up>", { noremap = true })
+remapp("c", "<C-.>", "<Down>", { noremap = true })
 
 -- <Ctrl><key> CMD movement remaps
 --remapp("c", "<C-l>", <Right>, {noremap = true})
@@ -225,9 +219,6 @@ remapp("c", "<C-.>", "<Down>", {noremap = true})
 --remapp("c", "<C-x>", <C-h>, {noremap = true})
 --remapp("c", "<C-h>", <Left>, {noremap = true})
 
-
 --
 -- Experimental Remaps
 --
-
-

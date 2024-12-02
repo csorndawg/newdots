07fb5cfbd30ab47829440e4215dc0b7f75000679
csorndawg <zaccsorny@yahoo.com>
2024-12-02 11:29:07 -0500

    Add abort autocompletion keymap. Add extra/user directories as case conditions.

--- bash.d/functions.bash
+++ bash.d/functions.bash
@@ -1,157 +1,152 @@
 # BASHRC Functions
 
 # TODO: add check to see if cwd within git repo, and if so quit gracefully
-gpou (){
-    git push -u origin $(git branch --show-current)
+gpou() {
+	git push -u origin $(git branch --show-current)
 }
 
-
-# git diff HEAD <X>  where X is 1 
-gdfhx (){
-    if [ -z "$1" ];
-    then
-        X=1
-    else
-        X="$1"
-    fi
-    git diff HEAD~$X
+# git diff HEAD <X>  where X is 1
+gdfhx() {
+	if [ -z "$1" ]; then
+		X=1
+	else
+		X="$1"
+	fi
+	git diff HEAD~$X
 }
 
 ## RSYNC HELPER FUNCTIONS
 
 ## rsync dir - dryrun
-rsdd (){
-    #echo "rsync --dry-run -azvphr"
-    rsync --dry-run -azvphr $1 $2
+rsdd() {
+	#echo "rsync --dry-run -azvphr"
+	rsync --dry-run -azvphr $1 $2
 }
 ## rsync dir - execute
-rsdx(){
-    rsync -azvphr $1 $2
+rsdx() {
+	rsync -azvphr $1 $2
 }
 ## rsync file - dryrun
-rsfd (){
-    rsync --dry-run -azvph $1 $2
+rsfd() {
+	rsync --dry-run -azvph $1 $2
 }
 ## rsync file - execute
-rsfx(){
-    rsync -azvphr $1 $2
+rsfx() {
+	rsync -azvphr $1 $2
 }
 
-
-mcd (){
-    DIRPATH="$1"
-    mkdir -p $DIRPATH && cd $DIRPATH
+mcd() {
+	DIRPATH="$1"
+	mkdir -p $DIRPATH && cd $DIRPATH
 }
 
-# recache git credentials -- temporary fix until I can find a way to 
+# recache git credentials -- temporary fix until I can find a way to
 # share $SSH_AUTH b/w tmux windows
-gcc (){
-    eval $(ssh-agent) && ssh-add "$HOME"/.ssh/id_rsa
-    git config credential.helper 'cache --timeout=3600'
+gcc() {
+	eval $(ssh-agent) && ssh-add "$HOME"/.ssh/id_rsa
+	git config credential.helper 'cache --timeout=3600'
 }
 
-
-## 
+##
 ## Scratch file generator
 ##
 
 ## qxs - (Q)ui(X) (S)cratch file genertor
-qxs(){
+qxs() {
 
-    # function must be given an ARG when called
-    # ARG must map to one of the below scratch "file flags" 
-    #
-    # function edits:
-    #   - bash sandbox (b)
-    #   - python sandbox (p)
-    #   - dotfiles sandbox (d)
-    #   - sql sandbox (s)
-    #   - testing sandbox ()
-    #   - junk sandbox (j)      ## @TODO - create script to auto remove files from folder after X time (> 1 week)
-    #   - git sandbox (g)
-    #   - written txt/markdown notes (w)   ## 'w' for written, use 'h' for human/help if 'w' taken
-    #
+	# function must be given an ARG when called
+	# ARG must map to one of the below scratch "file flags"
+	#
+	# function edits:
+	#   - bash sandbox (b)
+	#   - python sandbox (p)
+	#   - dotfiles sandbox (d)
+	#   - sql sandbox (s)
+	#   - testing sandbox ()
+	#   - junk sandbox (j)      ## @TODO - create script to auto remove files from folder after X time (> 1 week)
+	#   - git sandbox (g)
+	#   - written txt/markdown notes (w)   ## 'w' for written, use 'h' for human/help if 'w' taken
+	#
 
-    ## variables
-    SANDBOXES_HOME_DIR="$HOME/.local/var/temp/sandboxes"
-    TEXTFILES_HOME_DIR="$HOME/git/kb2/quick/autostash"  # diff location for written notes/docs
+	## variables
+	SANDBOXES_HOME_DIR="$HOME/.local/var/temp/sandboxes"
+	TEXTFILES_HOME_DIR="$HOME/git/kb2/quick/autostash" # diff location for written notes/docs
 
-    # set EDITOR to vim as fallback if DNE
-    #[ -z "$EDITOR" ] && EDITOR=vim
+	# set EDITOR to vim as fallback if DNE
+	#[ -z "$EDITOR" ] && EDITOR=vim
 
-    # scalably add/remove/modify variables dirpaths
-    VAR_LIST=($SANDBOXES_HOME_DIR $TEXTFILES_HOME_DIR)
-    for varx in ${VAR_LIST[@]}; do
-        [[ -d "$varx" ]] || mkdir -p "$varx"
-        mkdir -p $varx
-    done
+	# scalably add/remove/modify variables dirpaths
+	VAR_LIST=($SANDBOXES_HOME_DIR $TEXTFILES_HOME_DIR)
+	for varx in ${VAR_LIST[@]}; do
+		[[ -d "$varx" ]] || mkdir -p "$varx"
+		mkdir -p $varx
+	done
 
-    ## make subdirs first 
-    SUBDIRS=(bash python dotfiles git sql junk testing all)
-    for sd in ${SUBDIRS[@]}; do
-        mkdir -p "$SANDBOXES_HOME_DIR/$sd"
-    done
+	## make subdirs first
+	SUBDIRS=(bash python dotfiles git sql junk testing all)
+	for sd in ${SUBDIRS[@]}; do
+		mkdir -p "$SANDBOXES_HOME_DIR/$sd"
+	done
 
+	# @TODO - Add upper/lower case variations to CASE statement for existing flags
+	# create scratchfile from CASE statement flag input
+	case "$1" in
 
-    # @TODO - Add upper/lower case variations to CASE statement for existing flags
-    # create scratchfile from CASE statement flag input
-    case "$1" in
+	b | B | bash | sh | shell)
+		echo -n "Bash/Shell sandbox FLAG passed"
+		mkdir -p "$SANDBOXES_HOME_DIR/bash"
+		$EDITOR "$SANDBOXES_HOME_DIR/bash/bash_scratch_$(date +%Y%m%d-%H%M%S).sh"
+		;;
 
-        b|B|bash|sh|shell)
-            echo -n "Bash/Shell sandbox FLAG passed"
-            mkdir -p "$SANDBOXES_HOME_DIR/bash"
-            $EDITOR "$SANDBOXES_HOME_DIR/bash/bash_scratch_$(date +%Y%m%d-%H%M%S).sh"
-        ;;
+	p | P | python)
+		echo -n "Python sandbox FLAG passed"
+		mkdir -p "$SANDBOXES_HOME_DIR/python"
+		$EDITOR "$SANDBOXES_HOME_DIR/python/python_scratch_$(date +%Y%m%d-%H%M%S).py"
+		;;
 
-        p|P|python)
-            echo -n "Python sandbox FLAG passed"
-            mkdir -p "$SANDBOXES_HOME_DIR/python"
-            $EDITOR "$SANDBOXES_HOME_DIR/python/python_scratch_$(date +%Y%m%d-%H%M%S).py"
-        ;;
-        
-        d|dotfiles)
-            echo -n "Dotfile sandbox FLAG passed. Append extension manually if filetype extention needed."
-            mkdir -p "$SANDBOXES_HOME_DIR/dotfiles"
-            $EDITOR "$SANDBOXES_HOME_DIR/dotfiles/dotfile_scratch_$(date +%Y%m%d-%H%M%S).dotfile"
-        ;;
-        
-        g|git)
-            echo -n "Git sandbox FLAG passed"
-            mkdir -p "$SANDBOXES_HOME_DIR/git"
-            $EDITOR "$SANDBOXES_HOME_DIR/git/git_scratch_$(date +%Y%m%d-%H%M%S).git"
-        ;;
-        
-        j|junk|temp|tmp|quick|q|J)
-            echo -n "Junk/Quick/Temp sandbox FLAG passed. Append extension manually if filetype extention needed."
-            mkdir -p "$SANDBOXES_HOME_DIR/junk"
-            $EDITOR "$SANDBOXES_HOME_DIR/junk/junk_scratch_$(date +%Y%m%d-%H%M%S).junk"
-        ;;
+	d | dotfiles)
+		echo -n "Dotfile sandbox FLAG passed. Append extension manually if filetype extention needed."
+		mkdir -p "$SANDBOXES_HOME_DIR/dotfiles"
+		$EDITOR "$SANDBOXES_HOME_DIR/dotfiles/dotfile_scratch_$(date +%Y%m%d-%H%M%S).dotfile"
+		;;
 
-        t|test|explore|experiment|exp)
-            echo -n "Test/Experiment/Explore sandbox FLAG passed. Append extension manually if filetype extention needed."
-            mkdir -p "$SANDBOXES_HOME_DIR/testing"
-            $EDITOR "$SANDBOXES_HOME_DIR/testing/test_scratch_$(date +%Y%m%d-%H%M%S).test"
-        ;;
+	g | git)
+		echo -n "Git sandbox FLAG passed"
+		mkdir -p "$SANDBOXES_HOME_DIR/git"
+		$EDITOR "$SANDBOXES_HOME_DIR/git/git_scratch_$(date +%Y%m%d-%H%M%S).git"
+		;;
 
-        s|sql)
-            echo -n " sandbox FLAG passed"
-            mkdir -p "$SANDBOXES_HOME_DIR/sql"
-            $EDITOR "$SANDBOXES_HOME_DIR/sql/sql_scratch_$(date +%Y%m%d-%H%M%S).sql"
-        ;;
+	j | junk | temp | tmp | quick | q | J)
+		echo -n "Junk/Quick/Temp sandbox FLAG passed. Append extension manually if filetype extention needed."
+		mkdir -p "$SANDBOXES_HOME_DIR/junk"
+		$EDITOR "$SANDBOXES_HOME_DIR/junk/junk_scratch_$(date +%Y%m%d-%H%M%S).junk"
+		;;
 
-        w|h)
-            echo -n "Written/Help FLAG passed"
-            mkdir -p "$TEXTFILES_HOME_DIR/qxs"
-            $EDITOR "$TEXTFILES_HOME_DIR/qxs/doc_scratch_$(date +%Y%m%d-%H%M%S).md"
-        ;;
+	t | test | explore | experiment | exp)
+		echo -n "Test/Experiment/Explore sandbox FLAG passed. Append extension manually if filetype extention needed."
+		mkdir -p "$SANDBOXES_HOME_DIR/testing"
+		$EDITOR "$SANDBOXES_HOME_DIR/testing/test_scratch_$(date +%Y%m%d-%H%M%S).test"
+		;;
 
-        *)
-            echo -n "Unknown/Unspecified which defaults to ALL"
-            mkdir -p "$TEXTFILES_HOME_DIR/all"
-            $EDITOR "$SANDBOXES_HOME_DIR/all/all_scratch_$(date +%Y%m%d-%H%M%S).all"
-    esac
+	s | sql)
+		echo -n " sandbox FLAG passed"
+		mkdir -p "$SANDBOXES_HOME_DIR/sql"
+		$EDITOR "$SANDBOXES_HOME_DIR/sql/sql_scratch_$(date +%Y%m%d-%H%M%S).sql"
+		;;
+
+	w | h)
+		echo -n "Written/Help FLAG passed"
+		mkdir -p "$TEXTFILES_HOME_DIR/qxs"
+		$EDITOR "$TEXTFILES_HOME_DIR/qxs/doc_scratch_$(date +%Y%m%d-%H%M%S).md"
+		;;
+
+	*)
+		echo -n "Unknown/Unspecified which defaults to ALL"
+		mkdir -p "$TEXTFILES_HOME_DIR/all"
+		$EDITOR "$SANDBOXES_HOME_DIR/all/all_scratch_$(date +%Y%m%d-%H%M%S).all"
+		;;
+	esac
 }
-
 
 # TODO
 #gpsu (){
@@ -161,69 +156,65 @@ qxs(){
 #
 #}
 
-edf(){
+edf() {
 
-    # function must be given an ARG when called
-    # ARG must map to one of the below "file flags" 
-    #
-    # function edits:
-    #   - bashrc (b)
-    #   - bashrc_wsl (bw)
-    #   - vimrc (v)
-    #   - vim_lib (vl)
-    #   - init.lua (n)
-    #   - neovim lsp (nl)
-    #   - neovim plugins (np)
-    #   - neovim opts (no)
-    #   - neovim keymaps (nk)
-    #   - tmux.conf (t)
-    #   - gitconfig (g)
+	# function must be given an ARG when called
+	# ARG must map to one of the below "file flags"
+	#
+	# function edits:
+	#   - bashrc (b)
+	#   - bashrc_wsl (bw)
+	#   - vimrc (v)
+	#   - vim_lib (vl)
+	#   - init.lua (n)
+	#   - neovim plugins (np)
+	#   - neovim extras (nx)
+	#   - neovim user (nu)
+	#   - tmux.conf (t)
+	#   - gitconfig (g)
 
-    # DOTFILE path depends on host machine
-    if [[ "$(echo $HOSTNAME | egrep -ci 'DESKTOP')" -ge 1 ]]; then
-        DOTFILE_HOME_DIR="$HOME/dotfiles"
-        echo "Current session is running on my Local machine - $HOSTNAME"
-    else
-        DOTFILE_HOME_DIR="$HOME/dotfiles"
-    fi
-    echo "DOTFILE_HOME_DIR:  $DOTFILE_HOME_DIR"
+	# DOTFILE path depends on host machine
+	if [[ "$(echo $HOSTNAME | egrep -ci 'DESKTOP')" -ge 1 ]]; then
+		DOTFILE_HOME_DIR="$HOME/dotfiles"
+		echo "Current session is running on my Local machine - $HOSTNAME"
+	else
+		DOTFILE_HOME_DIR="$HOME/dotfiles"
+	fi
+	echo "DOTFILE_HOME_DIR:  $DOTFILE_HOME_DIR"
 
-    case $1 in 
-        b)
-            vim "$DOTFILE_HOME_DIR/bash.d/bashrc"
-            ;;
-        bw)
-            vim "$DOTFILE_HOME_DIR/bash.d/bashrc_wsl"
-            ;;
-        t)
-            vim "$DOTFILE_HOME_DIR/tmux.d/tmux.conf"
-            ;;
-        g)
-            vim "$DOTFILE_HOME_DIR/git.d/gitconfig"
-            ;;
-        ni)
-            vim "$DOTFILE_HOME_DIR/nvim.d/init.lua"
-            ;;
-        no)
-            vim "$DOTFILE_HOME_DIR/nvim.d/lua/user/opts.lua"
-            ;;
-        nk)
-            vim "$DOTFILE_HOME_DIR/nvim.d/lua/user/keymaps.lua"
-            ;;
-        np)
-            vim "$DOTFILE_HOME_DIR/nvim.d/lua/plugins"
-            ;;
-        nl)
-            vim "$DOTFILE_HOME_DIR/nvim.d/lua/extra/lsp.lua"
-            ;;
-        v)
-            vim "$DOTFILE_HOME_DIR/vim.d/vimrc"
-            ;;
-        vl)
-            vim "$DOTFILE_HOME_DIR/vim.d/vim_lib"
-            ;;
-        **)
-            echo 'Unrecognized $ARG passed' 
-            ;;
-    esac
+	case $1 in
+	b)
+		vim "$DOTFILE_HOME_DIR/bash.d/bashrc"
+		;;
+	bw)
+		vim "$DOTFILE_HOME_DIR/bash.d/bashrc_wsl"
+		;;
+	t)
+		vim "$DOTFILE_HOME_DIR/tmux.d/tmux.conf"
+		;;
+	g)
+		vim "$DOTFILE_HOME_DIR/git.d/gitconfig"
+		;;
+	ni)
+		vim "$DOTFILE_HOME_DIR/nvim.d/init.lua"
+		;;
+	nu)
+		vim "$DOTFILE_HOME_DIR/nvim.d/lua/user"
+		;;
+	np)
+		vim "$DOTFILE_HOME_DIR/nvim.d/lua/plugins"
+		;;
+	nx)
+		vim "$DOTFILE_HOME_DIR/nvim.d/lua/extra"
+		;;
+	v)
+		vim "$DOTFILE_HOME_DIR/vim.d/vimrc"
+		;;
+	vl)
+		vim "$DOTFILE_HOME_DIR/vim.d/vim_lib"
+		;;
+	**)
+		echo 'Unrecognized $ARG passed'
+		;;
+	esac
 }
--- nvim.d/lua/extra/lsp.lua
+++ nvim.d/lua/extra/lsp.lua
@@ -2,129 +2,135 @@
 -- IDE-LIKE CONFIG LOGIC - LSP, CMP, DAP, ETC.
 --
 
-
 -- LSPCONFIG
 
--- require() imports "plugin/lspconfig.lua" 
-local nvimd_lsp = require('lspconfig')
- 
--- define LSP servers 
-local servers = { 'pyright', 'ansiblels', 'bashls', 'yamlls', 'docker_compose_language_service', 'dockerls' }
+-- require() imports "plugin/lspconfig.lua"
+local nvimd_lsp = require("lspconfig")
 
--- do things when language server attached to current buffer 
-local on_attach = function(client,bufnr)
+-- define LSP servers
+local servers = { "pyright", "ansiblels", "bashls", "yamlls", "docker_compose_language_service", "dockerls" }
 
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local opts = { noremap=true, silent=true }
+-- do things when language server attached to current buffer
+local on_attach = function(client, bufnr)
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
+	local opts = { noremap = true, silent = true }
 
-  -- first remap
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+	-- first remap
+	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+	buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+	buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+	buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+	buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+	buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+	buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 end
 
 -- set capabilities for ??
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 for _, lsp in ipairs(servers) do
-
 	-- nvimd_lsp[lsp].setup {}	-- installs LSP server, but will not be recognized unless manually referenced/called
 
 	-- run local on_attach function, which allows files to be
 	-- recognize/picked up by its LSP server (if exists), for each server
-	nvimd_lsp[lsp].setup {
-        capabilities = capabilities,
-		on_attach = on_attach ,
-	}
+	nvimd_lsp[lsp].setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+	})
 end
-
 
 ------------------------
 -- NVIM-CMP
 ------------------------
-local cmp = require('cmp')
+local cmp = require("cmp")
 --local lspkind = require('lspkind')
-local luasnip = require('luasnip')
+local luasnip = require("luasnip")
 
 -- better autocompletion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
-cmp.setup {
+cmp.setup({
 	-- Format the autocomplete menu
-    -- formatting = {
-    --		format = lspkind.cmp_format()
-    --	},
+	-- formatting = {
+	--		format = lspkind.cmp_format()
+	--	},
 
 	mapping = {
-        -- Use Tab and shift-Tab to navigate autocomplete menu
-        ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-	      luasnip.expand_or_jump()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end,
-        ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-        end,
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
+		-- Use Tab and shift-Tab to navigate autocomplete menu
+		["<Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end,
+		["<S-Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end,
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
 
-	-- @EXPERIMENTAL - Change to smoother keybinding after confirming this 
-	-- will not break anything.
-	--
-	-- <c-f> will move you (f)orward (to the right) of each expansion locations
-	-- <c-b> is similar, except moving you (b)ackwards.
-	['<C-f>'] = cmp.mapping(function()
-		if luasnip.expand_or_locally_jumpable() then
-			luasnip.expand_or_jump()
-		end
-	end, { 'i', 's' }),
-	['<C-b>'] = cmp.mapping(function()
-		if luasnip.locally_jumpable(-1) then
-			luasnip.jump(-1)
-		end
-	end, { 'i', 's' }),
-    },
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end
-    },
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'buffer' },
-        { name = 'path' },
-	{ name = 'dadbod' },
-        { name = 'cmdline' },
-    },
-}
+		-- @EXPERIMENTAL - Change to smoother keybinding after confirming this
+		-- will not break anything.
+		--
+		-- <c-f> will move you (f)orward (to the right) of each expansion locations
+		-- <c-b> is similar, except moving you (b)ackwards.
+		["<C-f>"] = cmp.mapping(function()
+			if luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			end
+		end, { "i", "s" }),
+
+		["<C-b>"] = cmp.mapping(function()
+			if luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end, { "i", "s" }),
+
+		["<C-Space>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.abort() -- Close the completion menu without selecting any suggestion
+			else
+				fallback() -- Fall back to the normal <C-Space> functionality
+			end
+		end, { "i", "c", "s" }),
+	},
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+		{ name = "dadbod" },
+		{ name = "cmdline" },
+	},
+})

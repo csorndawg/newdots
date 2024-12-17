3175681e527085840e49f56dd702ed5d4bf4f005
csorndawg <zaccsorny@yahoo.com>
2024-12-17 05:17:31 +0000

    Add secure shellGPT for ZSH and Bash shells

---
+++ bash.d/lib/shellGPT_conf.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# shellGPT secure configuration
+shellGPT_API_Key="$(cat ~/git/fortknox/secrets/shellGPT_API_Key.txt)"
+export OPENAI_API_KEY="$shellGPT_API_Key"
+
+#TODO: will add logic to allow for multiple/follow-up questions"
+sgptf() {
+	# accepts filepath ARG for multiline prompts
+	CHATGPT_PROMPT_FILE_PATH="$1"
+	CHATGPT_PROMPT_TEXT="$(cat $CHATGPT_PROMPT_FILE_PATH)"
+	sgpt $CHATGPT_PROMPT_TEXT
+}

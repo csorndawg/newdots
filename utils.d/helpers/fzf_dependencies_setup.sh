56b28ae6cf88d076bd5da5fd9aa59d749d6d9a63
csorndawg <zaccsorny@yahoo.com>
2025-01-01 12:41:17 -0500

    Update so fzf dependencies sourced directly from fzf.config

--- utils.d/helpers/fzf_dependencies_setup.sh
+++ utils.d/helpers/fzf_dependencies_setup.sh
@@ -13,7 +13,7 @@ function helper_fd {
 	echo "" && echo "Installing fd"
 	sudo apt install fd-find -y
 	ln -s "$(which fdfind)" "$HOME/.local/bin/fd"
-}
+}  
 
 function helper_bat {
 	# bat
@@ -44,7 +44,7 @@ function external_binaries_setup {
 				bat)
 					echo "Non-standard install"
 					echo "Installing $dpnd"
-					helper_fd
+					helper_bat
 					echo "$dpnd installed successfully"
 					;;
 				fd)
@@ -65,49 +65,9 @@ function external_binaries_setup {
 	echo "" && echo "All external dependencies installed"
 }
 
-# Ensure FZF key-bindings/completions file is available for current $SHELL
-# in expected dotfile repo folder so it can be sourced at runtime. Assumes 
-# OS is Ubuntu/Debian
-function fzf_shell_src_scripts {
-	# VARS
-	EXPECTED_FZF_SRC_FILES_DIR="$HOME/dotfiles/utils.d"
-
-	# set vars for shell type FZF source file path
-	if [[ "$(echo $SHELL | egrep -c "\/zsh$")" -ge 1 ]];
-	then
-		EXPECTED_FZF_KEYBINDS_SRC_PATH="$EXPECTED_FZF_SRC_FILES_DIR/fzf-key-bindings.zsh"
-		EXPECTED_FZF_COMP_SRC_PATH="$EXPECTED_FZF_SRC_FILES_DIR/fzf-completion.zsh"
-	else
-		EXPECTED_FZF_KEYBINDS_SRC_PATH="$EXPECTED_FZF_SRC_FILES_DIR/fzf-key-bindings.bash"
-		EXPECTED_FZF_COMP_SRC_PATH="$EXPECTED_FZF_SRC_FILES_DIR/fzf-completion.bash"
-	fi
-	
-	# check/debug VAR value
-	echo "" 
-	echo "FZF Key Bindings Source File:  $EXPECTED_FZF_KEYBINDS_SRC_PATH"
-	echo "FZF Completion Source File:  $EXPECTED_FZF_COMP_SRC_PATH"
-	echo ""
-
-	# put expected files in list after they are set and then check for existence
-	FZF_EXPECTED_FILES=("$EXPECTED_FZF_KEYBINDS_SRC_PATH" "$EXPECTED_FZF_COMP_SRC_PATH")
-	for file in ${FZF_EXPECTED_FILES[@]}; do 
-		echo "Checking if $file exists"
-		if [[ ! -f "$file" ]]; then
-			echo "ERROR - $file does not exist, and needs to be manually resolved"
-		else
-			echo "$file does exists" 
-		fi
-	done
-}
-
 ## ==================================
 ## MAIN
 ## ==================================
 
 # install external binary dependencies
-external_binaries_setup
-
-# setup FZF shell keybind/comp source scripts for Ubuntu OS
-fzf_shell_src_scripts
-
-echo "" && echo "DONE"
+external_binaries_setup 1> /dev/null || echo "Issue occurred during FZF dependency setup"

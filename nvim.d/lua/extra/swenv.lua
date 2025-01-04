d5ab88d412bd9302ace8036e6f5dca36b90a1370
csorndawg <zaccsorny@yahoo.com>
2025-01-03 19:49:15 -0500

    Picker still not working - committing current work and will revist later

--- nvim.d/lua/extra/swenv.lua
+++ nvim.d/lua/extra/swenv.lua
@@ -1,18 +1,17 @@
-require('swenv').setup({
-  -- Should return a list of tables with a `name` and a `path` entry each.
-  -- Gets the argument `venvs_path` set below.
-  -- By default just lists the entries in `venvs_path`.
-  get_venvs = function(venvs_path)
-    return require('swenv.api').get_venvs(venvs_path)
-  end,
+require("swenv").setup({
+	-- Should return a list of tables with a `name` and a `path` entry each.
+	-- Gets the argument `venvs_path` set below.
+	-- By default just lists the entries in `venvs_path`.
+	get_venvs = function(venvs_path)
+		return require("swenv.api").get_venvs(venvs_path)
+	end,
 
-  -- Path passed to `get_venvs`.
-  -- venv xdg: '~/.local/state/python/venvs'
-  venvs_path = vim.fn.expand('~/venvs'),
-  -- Something to do after setting an environment, for example call vim.cmd.LspRestart
-  post_set_venv = nil,
+	-- Path passed to `get_venvs`.
+	-- venv xdg: '~/.local/state/python/venvs'
+	venvs_path = vim.fn.expand("~/.local/state/python/venvs"),
+	-- Something to do after setting an environment, for example call vim.cmd.LspRestart
+	post_set_venv = nil,
 })
-
 
 -- swenv remaps
 --   follow <leader>e<mnemonic> remap pattern
@@ -20,6 +19,10 @@ require('swenv').setup({
 -- remapping examples
 --   vim.keymap.set("n", "<LHS>", "<RHS>", { noremap = true })
 --   vim.keymap.set("n", "gp", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", { noremap = true })
-vim.keymap.set("n", "<Leader>ec", "<cmd>lua require('swenv.api').get_current_venv()<CR>", { noremap = true })
-vim.keymap.set("n", "<Leader>ep", "<cmd>lua require('swenv.api').pick_venv()<CR>", { noremap = true })
 
+--   (e)nvironement (c)urrent
+vim.keymap.set("n", "<Leader>ec", "<cmd>lua require('swenv.api').get_venvs()<CR>", { noremap = true })
+--   (e)nvironement (v)iew
+vim.keymap.set("n", "<Leader>ev", "<cmd>lua require('swenv.api').get_current_venv()<CR>", { noremap = true })
+--   (e)nvironement (p)ick
+vim.keymap.set("n", "<Leader>ep", "<cmd>lua require('swenv.api').pick_venv()<CR>", { noremap = true })

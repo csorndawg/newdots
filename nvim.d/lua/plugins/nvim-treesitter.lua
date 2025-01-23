d201230ddce45cd099fcf848f37b3207f9fceca3
csorndawg <zaccsorny@yahoo.com>
2025-01-23 00:38:39 -0500

    Add more TS langs

--- nvim.d/lua/plugins/nvim-treesitter.lua
+++ nvim.d/lua/plugins/nvim-treesitter.lua
@@ -1,44 +1,51 @@
 return {
-  -- { "nvim-treesitter/nvim-treesitter", }
+	-- { "nvim-treesitter/nvim-treesitter", }
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { 
-           "bash",
-           "c",
-           "csv",
-           "diff",
-           "dockerfile",
-           "git_config",
-           "gitcommit",
-           "gitignore",
-           "html",
-           "ini",
-           "jq",
-           "json",
-           "lua",
-           "python",
-           "regex",
-           "sql",
-           "ssh_config",
-           "toml",
-           "vim",
-           "vimdoc",
-           "xml",
-           "yaml",
-         --"",
-         --"",
-         --"" 
-        },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-    end
- }
+			configs.setup({
+				ensure_installed = {
+					"awk",
+					"bash",
+					"csv",
+					"diff",
+					"dockerfile",
+					"git_config",
+					"gitcommit",
+					"gitignore",
+					"groovy",
+					"html",
+					"ini",
+					"jq",
+					"json",
+					"lua",
+					"luadoc",
+					"make",
+					"markdown",
+					"printf",
+					"promql",
+					"python",
+					"regex",
+					"requirements",
+					"sql",
+					"ssh_config",
+					"terraform",
+					"tmux",
+					"toml",
+					"tsv",
+					"vim",
+					"vimdoc",
+					"xml",
+					"yaml",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }

8a980a0fa5cd7d53ea23ceeaa5148ef46cb03586
csorndawg <zaccsorny@yahoo.com>
2025-01-25 02:47:53 -0500

    Commit progress w/ conform custom conf - leader-f mapping still causing issues and needs more work

--- nvim.d/lua/extra/conform.lua
+++
@@ -1,8 +0,0 @@
--- @WIP
--- Keymaps to toggle formatting. Use below `lint.lua` remap example as reference.
---vim.api.nvim_set_keymap("n", "<leader>li", ":lua update_lint_level('info')<CR>", { noremap = true, silent = true })
-
---
-require("conform")
-vim.api.nvim_set_keymap("n", "<leader>fd", ":FormatDisable <CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fe", "<cmd>FormatEnable<CR>", { noremap = true, silent = true })

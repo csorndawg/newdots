cf0a794f538a07e1c11d1bdb720eb40d5c450565
csorndawg <zaccsorny@yahoo.com>
2025-01-28 18:19:06 -0500

    Add FZF comp to batch of shell aliases and commands

--- bash.d/lib/custom/comps/fzfcomp_extras.bash
+++ bash.d/lib/custom/comps/fzfcomp_extras.bash
@@ -1,12 +1,22 @@
 # Extends FZF_COMP functionality to specified custom aliases
 
-
 # @FIXME: Below works for some of use cases not all, needs logic improvement
-# example: grss wont work if you've removed the file since FZF_COMP_PATH wont see it 
+# example: grss wont work if you've removed the file since FZF_COMP_PATH wont see it
 # ... need actual git functions
 
-GIT_ALIASES_LIST=(grm ga gd gstp grs grss gds gdm gdms gdo gdom gstpp)
-for ga in ${GIT_ALIASES_LIST[@]}; do 
-  echo "_fzf_setup_completion path $ga"
-  _fzf_setup_completion path $ga
+GIT_ALIASES_LIST=(grm ga gd gstp grs grss gsta gds gdm gdms gdo gdom)
+for ga in ${GIT_ALIASES_LIST[@]}; do
+	#_fzf_setup_completion path "$ga"
+	echo "complete -F __fzf_default_completion $ga"
+	complete -F __fzf_default_completion "$ga"
+done
+
+# TODO: Add config file that extends FZF default shell comp to any command/alias in the file. After
+# making the file, move any command/alias defined below to the file to clean up this script.
+# Enable FZF default shell completion for shell commands and aliases
+FZF_DEFAULT_COMP_EXTENSION_LIST=$(tee rgv rgi wc rsync zip)
+for item in ${FZF_DEFAULT_COMP_EXTENSION_LIST[@]}; do
+	echo "item: $item"
+	echo "complete -F __fzf_default_completion $item"
+	complete -F __fzf_default_completion $item
 done

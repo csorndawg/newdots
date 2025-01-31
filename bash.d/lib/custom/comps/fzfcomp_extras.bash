1b869b2b6e2f2f9cfc29606a60134cd150fb5b40
csorndawg <zaccsorny@yahoo.com>
2025-01-30 22:59:52 -0500

    Git aliases shell __git_complete comp restored

--- bash.d/lib/custom/comps/fzfcomp_extras.bash
+++ bash.d/lib/custom/comps/fzfcomp_extras.bash
@@ -1,12 +1,24 @@
 # Extends FZF_COMP functionality to specified custom aliases
 
+source $HOME/dotfiles/bash.d/lib/custom/comps/aliases/git.comp
+
 # @FIXME: Below works for some of use cases not all, needs logic improvement
 # example: grss wont work if you've removed the file since FZF_COMP_PATH wont see it
 # ... need actual git functions
 
-GIT_ALIASES_LIST=(grm ga gd gstp grs grss gsta gds gdm gdms gdo gdom)
+#GIT_ALIASES_LIST=(grm ga gd gstp grs grss gsta gds gdm gdms gdo gdom)
+GIT_ALIASES_LIST=()
+#TEMP_SKIP_LIST=(ga)
 for gaf in ${GIT_ALIASES_LIST[@]}; do
-	#_fzf_setup_completion path "$gaf"
+
+	# need to skip certain aliases until I can get both git branch/path
+	# completion AND FZF_PATH comp working together
+	for skip_file in ${TEMP_SKIP_LIST[@]}; do
+		if [[ "$skip_file" == "$gaf" ]]; then
+			__git_complete "$gaf" _git_add
+		#	continue
+		fi
+	done
 	echo "complete -F __fzf_default_completion $gaf"
 	#complete -o bashdefault -o default -F _fzf_path_completion $cga
 	complete -F __fzf_default_completion "$gaf"

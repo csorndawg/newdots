8639770125f2d973bfcca4d40fe0491a577fe3b4
csorndawg <zaccsorny@yahoo.com>
2025-02-01 15:24:14 -0500

    Comment out bad git fzf completion function aliases

--- bash.d/lib/custom/comps/functs/custom_git_comp_functs.sh
+++ bash.d/lib/custom/comps/functs/custom_git_comp_functs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# @BROKEN: FZF comp x. alias integration is broken and needs to remain commented out until fixed
+
 # How to assign completion: complete -F _custom_git_diff <target>
 # Example: complete -F _custom_git_diff_files gd
 
@@ -37,7 +39,7 @@ _custom_git_diff() {
 }
 
 # shows `git diff` completion options for both branches and files
-complete -F _custom_git_diff gd
+#complete -F _custom_git_diff gd
 
 _custom_git_selective_stash() {
 	local message files
@@ -89,8 +91,8 @@ _custom_git_selective_stash() {
 	echo "Stashed selected files."
 }
 
-complete -F _custom_git_selective_stash gstp
-complete -F _custom_git_selective_stash gst
+#complete -F _custom_git_selective_stash gstp
+#complete -F _custom_git_selective_stash gst
 
 # @TODO: Build out rest of Git alias custom completion functions
 # custom git branch aliases completion logic
@@ -111,10 +113,7 @@ _custom_git_remove() {
 	fi
 	echo "Removed selected files."
 }
-complete -F _custom_git_remove grm
-
-# custom git reflog aliases completion logic
-#_custom_git_reflog() {}
+#complete -F _custom_git_remove grm
 
 # custom git merge aliases completion logic
 _custom_git_merge() {
@@ -122,7 +121,7 @@ _custom_git_merge() {
 	# shows diffstat b/w FZF spinner branch and current branch
 	git branch -l | rg -v '\*|main' | awk '{print $1}' | fzf --exit-0 --select-1 --header "Select branch to merge into current working branch" --preview "git diff --stat {} $(git branch --show-current)"
 }
-complete -F _custom_git_selective_stash gm
+#complete -F _custom_git_selective_stash gm
 
 # custom git add aliases completion logic
 _custom_git_add() {
@@ -146,5 +145,5 @@ _custom_git_add() {
 		fi
 	done <<<"$files"
 }
-complete -F _custom_git_add ga
-complete -F _custom_git_add gad
+#complete -F _custom_git_add ga
+#complete -F _custom_git_add gad
--- wsl/todo_wsl.md
+++
@@ -1,8 +0,0 @@
-
-# WSL cleanup/todo tasks
-
-- Fix FZF ***
-- remove annoying warning/non failure errors from bashrc sourcing like curses gem
-- refactor remaining WSL only logic still in bashrc_wsl 
-
-

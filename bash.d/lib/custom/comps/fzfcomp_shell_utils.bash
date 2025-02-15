a01e682dc4e1f69f2bfa5a4e80aa2b516b2c0d0c
csorndawg <zaccsorny@yahoo.com>
2025-02-15 12:03:43 -0500

    Add custom python argparse luansip snippet. Add new fzfcomp file for bash fuzzy comp. Add section to new fzfcomp for commands that fixed previously broken cmds (python3, echo, etc.) and added section for new custom comp with sample ssh notrigger

---
+++ bash.d/lib/custom/comps/fzfcomp_shell_utils.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Description of the script.
+
+## vanilla comp enablement (just enables default defined in fzf.config)
+## batch1: echo/python/3//source/patch
+complete -F __fzf_default_completion echo
+complete -F __fzf_default_completion python
+complete -F __fzf_default_completion python3
+complete -F __fzf_default_completion source
+complete -F __fzf_default_completion patch
+
+## extra special comp enablement
+_fzf_complete_special_notrigger() {
+	FZF_COMPLETION_TRIGGER='' _fzf_host_completion
+}
+complete -o bashdefault -o default -F _fzf_complete_special_notrigger ssh
--- nvim.d/lua/extra/luaSnipsCustom/python_custom.lua
+++ nvim.d/lua/extra/luaSnipsCustom/python_custom.lua
@@ -25,12 +25,39 @@ local conds_expand = require("luasnip.extras.conditions.expand")
 
 -- python sample
 ls.add_snippets("python", {
-	s("hello_looahh", {
+	s("hello_looahh_py", {
 		t(
 			'print(f"This was generated from my custom LuaSnips snippet located in nvim.d/lua/extra/luasnips_custom/python_custom.lua snippet")'
 		),
 	}),
 	s("main", {
 		t("def main():", "\n", "\t pass"),
+	}),
+
+	-- argparse prelim
+	s("argparse", {
+		t({ "import argparse", "", "parser = argparse.ArgumentParser()" }),
+		t({ "", "parser.add_argument('-" }),
+		i(1, "a"),
+		t({ "', '--" }),
+		i(2, "arg1"),
+		t({ "', help='" }),
+		i(3, "Help for argument 1"),
+		t({ "')" }),
+		t({ "", "parser.add_argument('-" }),
+		i(4, "b"),
+		t({ "', '--" }),
+		i(5, "arg2"),
+		t({ "', help='" }),
+		i(6, "Help for argument 2"),
+		t({ "')" }),
+		t({ "", "parser.add_argument('-" }),
+		i(7, "c"),
+		t({ "', '--" }),
+		i(8, "arg3"),
+		t({ "', help='" }),
+		i(9, "Help for argument 3"),
+		t({ "')" }),
+		t({ "", "", "args = parser.parse_args()" }),
 	}),
 })

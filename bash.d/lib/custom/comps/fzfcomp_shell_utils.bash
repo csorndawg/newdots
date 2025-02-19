894c2733b44937d493c94eb9080c95bc3a586267
csorndawg <zaccsorny@yahoo.com>
2025-02-19 02:48:57 -0500

    Fixed error. Removed custom FZF comp enablement, since function at end of bashrc applies it to all commands by default.

--- bash.d/lib/custom/comps/fzfcomp_shell_utils.bash
+++ bash.d/lib/custom/comps/fzfcomp_shell_utils.bash
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
-#
-# Description of the script.
-
-## vanilla comp enablement (just enables default defined in fzf.config)
-## batch1: echo/python/3//source/patch
-complete -F __fzf_default_completion echo
-complete -F __fzf_default_completion python
-complete -F __fzf_default_completion python3
-complete -F __fzf_default_completion bash
-complete -F __fzf_default_completion source
-complete -F __fzf_default_completion ansible
-complete -F __fzf_default_completion ansible-playbook
-complete -F __fzf_default_completion patch
-complete -F __fzf_default_completion diff
 
 ## extra special comp enablement
 _fzf_complete_special_notrigger() {

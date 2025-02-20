0174e831105c4d81e13a19612df01ec274d77a54
csorndawg <zaccsorny@yahoo.com>
2025-02-20 05:01:45 +0000

    Fix 'z' installer script/sourcing code

--- utils.d/dependencies/z_install.sh
+++ utils.d/dependencies/z_install.sh
@@ -2,23 +2,30 @@
 
 Z_XDG_DATA_DIR="$HOME/.local/share/z"
 Z_DATA="$Z_XDG_DATA_DIR/z_data"
-#export _Z_DATA="$Z_DATA"
-mkdir -p "$Z_XDG_DATA_DIR"
-touch "$Z_DATA"
-#export _Z_DATA="$Z_DATA"
-mkdir -p "$Z_XDG_DATA_DIR"
-touch "$Z_DATA"
+Z_SRC_REPO="$Z_XDG_DATA_DIR/src"
 
-# get clone and move to expected local dir spot
-git clone https://github.com/rupa/z.git
-mv -v z "$Z_XDG_DATA_DIR"
-mv -v z $Z_XDG_DATA_DIR
-echo ""
-# echo mv -v z "$Z_XDG_DATA_DIR/src"
-mv -v z "$Z_XDG_DATA_DIR/src"
+#export _Z_DATA="$Z_DATA"
+if [ -d "$Z_XDG_DATA_DIR" ]; then
+echo "$Z_XDG_DATA_DIR exists" 
+exit
+else 
+mkdir -p "$Z_XDG_DATA_DIR" 
+touch "$Z_DATA"
+fi
+
+# create temp workdir
+if [ -d "$(pwd)/.tmp/z" ]; then
+  rm -rf "$(pwd)/.tmp/z"
+fi
+cd "$(pwd)/.tmp"
+
+# clone 'z' repo, move to expected local dir spot, and set perms for symlin
+git clone https://github.com/rupa/z.git && echo ""
+mv -f z "$Z_SRC_REPO"
+find "$Z_SRC_REPO" -type f -exec chmod +x {} \;
 
 # make symlink to binary and source symlink
-# echo ln -sf $HOME/.local/share/z/src/z.sh $HOME/.local/bin/z.sh
+echo ln -sf $HOME/.local/share/z/src/z.sh $HOME/.local/bin/z.sh
 ln -sf $HOME/.local/share/z/src/z.sh $HOME/.local/bin/z.sh
 
 # add below to zsh/bash rc

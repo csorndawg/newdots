0d73ac6e8c46f52b2645c16020331caca362da3d
csorndawg <zaccsorny@yahoo.com>
2025-02-20 04:19:01 +0000

    Debugging skip return

--- utils.d/extend-fzf-comp-setup-cmds.bash
+++ utils.d/extend-fzf-comp-setup-cmds.bash
@@ -4,12 +4,15 @@
 # setup FZF path comp (file and dir paths) for all shell commands in list
 FZF_DEFAULT_PATH_COMP_EXTENSION_LIST=(tee kubectl rg fd nv patch python3 python bash source wc rsync zip)
 for item in ${FZF_DEFAULT_PATH_COMP_EXTENSION_LIST[@]}; do
+	echo ""
 	echo "item: $item"
 	exist_locally_check=$(which "$item" | wc -c)
 	if $(( $exist_locally_check > 0 )); 
 	then
 	  _fzf_setup_completion path $item
 	else
+	  echo "item: $item"
+	  echo "exist_locally_check: $exist_locally_check"
 		echo "Skipping $item b/c its not a command or does not exist on machine"
 	fi
 	# echo "complete -F __fzf_default_completion $item"
@@ -20,12 +23,15 @@ done
 # enable fzf comp with dir path only for all shell commands below 
 FZF_DEFAULT_DIR_COMP_EXTENSION_LIST=(tree)
 for item in ${FZF_DEFAULT_DIR_COMP_EXTENSION_LIST[@]}; do
+	echo ""
 	echo "item: $item"
 	exist_locally_check=$(which "$item" | wc -c)
 	if $(( $exist_locally_check > 0 )); 
 	then
 	  _fzf_setup_completion dir $item
 	else
+	  echo "item: $item"
+	  echo "exist_locally_check: $exist_locally_check"
 		echo "Skipping $item b/c its not a command or does not exist on machine"
 	fi
 done

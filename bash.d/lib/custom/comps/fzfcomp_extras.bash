0983d376f132685ae16bf1197403c0b2345ea2e0
csorndawg <zaccsorny@yahoo.com>
2025-02-20 04:13:15 +0000

    Untested fix

--- bash.d/lib/custom/comps/fzfcomp_extras.bash
+++ bash.d/lib/custom/comps/fzfcomp_extras.bash
@@ -1,35 +1,31 @@
 # Extends FZF_COMP functionality to specified custom aliases
 
-source $HOME/dotfiles/bash.d/lib/custom/comps/aliases/git.comp
 
-# @FIXME: Below works for some of use cases not all, needs logic improvement
-# example: grss wont work if you've removed the file since FZF_COMP_PATH wont see it
-# ... need actual git functions
-
-#GIT_ALIASES_LIST=(grm ga gd gstp grs grss gsta gds gdm gdms gdo gdom)
-GIT_ALIASES_LIST=()
-#TEMP_SKIP_LIST=(ga)
-for gaf in ${GIT_ALIASES_LIST[@]}; do
-
-	# need to skip certain aliases until I can get both git branch/path
-	# completion AND FZF_PATH comp working together
-	for skip_file in ${TEMP_SKIP_LIST[@]}; do
-		if [[ "$skip_file" == "$gaf" ]]; then
-			__git_complete "$gaf" _git_add
-		#	continue
-		fi
-	done
-	echo "complete -F __fzf_default_completion $gaf"
-	#complete -o bashdefault -o default -F _fzf_path_completion $cga
-	complete -F __fzf_default_completion "$gaf"
+# setup FZF path comp (file and dir paths) for all shell commands in list
+FZF_DEFAULT_PATH_COMP_EXTENSION_LIST=(tee kubectl rg fd nv patch python3 python bash source wc rsync zip)
+for item in ${FZF_DEFAULT_PATH_COMP_EXTENSION_LIST[@]}; do
+	echo "item: $item"
+	exist_locally_check=$(which "$item" | wc -c)
+	if $(( $exist_locally_check > 0 )); 
+	then
+	  _fzf_setup_completion path $item
+	else
+		echo "Skipping $item b/c its not a command or does not exist on machine"
+	fi
+	# echo "complete -F __fzf_default_completion $item"
+	# complete -F __fzf_default_completion $item
 done
 
-# TODO: Add config file that extends FZF default shell comp to any command/alias in the file. After
-# making the file, move any command/alias defined below to the file to clean up this script.
-# Enable FZF default shell completion for shell commands and aliases
-FZF_DEFAULT_COMP_EXTENSION_LIST=(tee rgv rgi wc rsync zip)
-for item in ${FZF_DEFAULT_COMP_EXTENSION_LIST[@]}; do
+
+# enable fzf comp with dir path only for all shell commands below 
+FZF_DEFAULT_DIR_COMP_EXTENSION_LIST=(tree)
+for item in ${FZF_DEFAULT_DIR_COMP_EXTENSION_LIST[@]}; do
 	echo "item: $item"
-	echo "complete -F __fzf_default_completion $item"
-	complete -F __fzf_default_completion $item
+	exist_locally_check=$(which "$item" | wc -c)
+	if $(( $exist_locally_check > 0 )); 
+	then
+	  _fzf_setup_completion dir $item
+	else
+		echo "Skipping $item b/c its not a command or does not exist on machine"
+	fi
 done

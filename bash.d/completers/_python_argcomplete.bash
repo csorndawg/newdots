f3f3b5a4134dfb167ec014c25fe360645cf47341
csorndawg <zaccsorny@yahoo.com>
2025-02-26 23:56:04 -0500

    Fix argcomplete uv error. Add sudo alias with tab comp.

--- bash.d/completers/_python_argcomplete.bash
+++ bash.d/completers/_python_argcomplete.bash
@@ -1,4 +1,5 @@
 
+# @BROKEN: Need to fix after reviewing UV docs
 # Begin added by argcomplete
-source "$HOME/.local/share/uv/tools/argcomplete/lib/python3.12/site-packages/argcomplete/bash_completion.d/_python-argcomplete"
+#source "$HOME/.local/share/uv/tools/argcomplete/lib/python3.12/site-packages/argcomplete/bash_completion.d/_python-argcomplete"
 # End added by argcomplete

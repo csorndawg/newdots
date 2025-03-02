645cea87f4b1e60063df7afe7481b82768c01483
csorndawg <zaccsorny@yahoo.com>
2025-03-02 16:58:40 -0500

    Open help in vert split config - partially complete

---
+++ nvim.d/lua/extra/telescope/telescope_actions.lua
@@ -0,0 +1,18 @@
+-- Modify Telescope's `help_tags` action to use vertical split
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+require("telescope").setup({
+	pickers = {
+		help_tags = {
+			attach_mappings = function(_, map)
+				map("i", "<CR>", function(prompt_bufnr)
+					local selection = action_state.get_selected_entry()
+					actions.close(prompt_bufnr)
+					vim.cmd("vertical rightbelow help " .. selection.value) -- Open help in vertical split
+				end)
+				return true
+			end,
+		},
+	},
+})

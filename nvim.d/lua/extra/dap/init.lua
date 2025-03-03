64d64bf1c1ed08883d1c7c83c66e2a141211595f
csorndawg <zaccsorny@yahoo.com>
2025-03-03 17:12:29 -0500

    Still need more work to finish but I can see most of the dap modules are sourced correctly from :help <tab>.
    Still debugging/tweaking with python basecase.

--- nvim.d/lua/extra/dap/init.lua
+++ nvim.d/lua/extra/dap/init.lua
@@ -11,3 +11,13 @@ local dap_configs = {
 	"extra.dap.dap-keybinds",
 	"extra.dap.python",
 }
+
+-- load each module from above table
+for _, module in ipairs(dap_configs) do
+	local ok, mod = pcall(require, module)
+	if ok and type(mod) == "table" and mod.setup then
+		mod.setup()
+	elseif not ok then
+		vim.notify("Failed to load " .. module, vim.log.levels.WARN)
+	end
+end

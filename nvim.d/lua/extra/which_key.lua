3f739a8695abe91b9fed56a10613d23f9e65f7ac
csorndawg <zaccsorny@yahoo.com>
2025-03-04 01:03:16 -0500

    Update whichkey to remove any keymap without 'desc=' option. This remove keymaps that were cluttering up whichkey map from whichkey map while still being able to run the keymap itself

--- nvim.d/lua/extra/which_key.lua
+++ nvim.d/lua/extra/which_key.lua
@@ -2,21 +2,11 @@
 
 local wk = require("which-key")
 
---[[
-    @TODO: Add/Remove/Update Which-Key Bindings List - includes keygroups and indv. keymaps
-
-    -- Key Groups (see "Search" key group for ref.)
-	Buffers: <leader>b
-	Debug: <leader>d
-	Trouble: <leader>t
-	Git: <leader>g
-	LSP: <leader>l
-
-    -- Indvidual Bindings:
-	<list here>
---]]
-
 wk.add({
+
+	-- @INFO: Whichkey Group/Subgroup Labels
+	-- -- only list labels, not actual keybindings themselves (save that for vim.keymap.set())
+
 	-- Telescope [s]earch keymap group
 	{ "<leader>s", group = "Search" }, -- telescope [s]earch group
 	{ "<leader>sn", desc = "Telescope find Nvim files" },
@@ -32,13 +22,13 @@ wk.add({
 	-- DAP [d]ebug keymap group
 	{ "<leader>d", group = "Debug" },
 	{ "<leader>db ", desc = "Toggle Breakpoint" },
-	{ "<leader>dB", desc = "Set Breakpoint" },
-	{ "<leader>dk ", desc = "Step Back" },
-	{ "<leader>dc", desc = "Continue" },
-	{ "<leader>dd", desc = "Disconnect" },
-	{ "<leader>dg", desc = "Get Session" },
-	{ "<leader>ds", desc = "Start" },
-	{ "<leader>dp", desc = "Pause" },
+	-- { "<leader>dB", desc = "Set Breakpoint" },
+	-- { "<leader>dk ", desc = "Step Back" },
+	-- { "<leader>dc", desc = "Continue" },
+	-- { "<leader>dd", desc = "Disconnect" },
+	-- { "<leader>dg", desc = "Get Session" },
+	-- { "<leader>ds", desc = "Start" },
+	-- { "<leader>dp", desc = "Pause" },
 	--{ "<leader>di ", desc = "Step Into" },
 	--{ "<leader>do ", desc = "Step Out" },
 	--{ "<leader>dC", desc = "Run To Cursor" },
@@ -62,10 +52,10 @@ wk.add({
 	{ "<leader>gh", group = "Hunk navigation" },
 
 	-- Mini
-	{ "<leader>m", group = "Mini" },
-	{ "<leader>ms", group = "[S]urround" },
-	{ "<leader>mc", group = "[C]omment" },
-	{ "<leader>ma", group = "[A]i" },
+	-- { "<leader>M", group = "Mini" },
+	-- { "<leader>Ms", group = "[S]urround" },
+	-- { "<leader>Mc", group = "[C]omment" },
+	-- { "<leader>Ma", group = "[A]i" },
 
 	-- LSP
 	{ "<leader>l", group = "LSP" },
@@ -77,18 +67,32 @@ wk.add({
 	{ "<leader>v", group = "Visual Mode" },
 
 	-- Trouble
-	{ "<leader>t", group = "Trouble" },
-})
+	{ "<leader>T", group = "Trouble" },
 
+	-- @INFO: Disable Keymaps Section
+	-- -- Hide keymaps specified below cfrom WhichKey map (keymap still exists, just wont appear )
+	--
+
+	{ "<leader>h", hidden = true }, -- hide this keymap
+	{ "<leader>j", hidden = true }, -- hide this keymap
+	{ "<leader>k", hidden = true }, -- hide this keymap
+	{ "<leader>l", hidden = true }, -- hide this keymap
+	{ "<leader>w", hidden = true }, -- hide this keymap
+	{ "<leader>W", hidden = true }, -- hide this keymap
+	{ "<leader>u", hidden = true }, -- hide this keymap
+	{ "<leader>q", hidden = true }, -- hide this keymap
+	{ "<leader>Q", hidden = true }, -- hide this keymap
+	{ "<leader>m", hidden = true }, -- hide this keymap
+})
 
 -- -- create 'ignore" (do not display in whichkey-menu) register
 -- wk.register({
 --   ["\\"] = {},  -- This hides all mappings starting with '\'
 -- })
 
--- alt. mapping to view keymaps 
+-- alt. mapping to view keymaps
 vim.keymap.set("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "view all whichkey keymaps" })
 
 vim.keymap.set("n", "<leader>wq", function()
-  vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
+	vim.cmd("WhichKey " .. vim.fn.input("WhichKey: "))
 end, { desc = "whichkey query lookup" })

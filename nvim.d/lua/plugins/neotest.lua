896883f9a38aa907b725df0ca73bad3f4c3071c1
csorndawg <zaccsorny@yahoo.com>
2025-03-18 22:29:56 -0400

    Add neotest skeleton - still need to test and tweak - starting with neotest python for now

--- nvim.d/lua/plugins/neotest.lua
+++ nvim.d/lua/plugins/neotest.lua
@@ -1,13 +1,26 @@
 return {
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"nvim-lua/plenary.nvim",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-neotest/neotest-python",
-			"nvim-neotest/neotest-bash",
-		},
+	"nvim-neotest/neotest",
+	dependencies = {
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-lua/plenary.nvim",
+		"nvim-neotest/neotest-bash", -- future adapter ex.
+		"nvim-neotest/neotest-python",
+		"nvim-neotest/nvim-nio",
+		"nvim-treesitter/nvim-treesitter",
 	},
+	opts = function()
+		local adapters = {}
+
+		-- Load Python adapter
+		local neotest_python = require("extra.dap.neotest_python").setup()
+		if neotest_python then
+			table.insert(adapters, neotest_python)
+		end
+
+		-- You can add more adapters like neotest-bash here in the future
+
+		return {
+			adapters = adapters,
+		}
+	end,
 }
---
+++ nvim.d/lua/plugins/swenv.nvim.lua
@@ -0,0 +1,9 @@
+return {
+	"AckslD/swenv.nvim",
+	opts = {
+		post_set_venv = function(venv_path)
+			vim.g.python3_host_prog = venv_path .. "/bin/python"
+		end,
+		venv_dirs = { "~/.local/state/uv/uvv" }, -- Adjust this based on where uv stores environments
+	},
+}

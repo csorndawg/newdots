3b8d674233ebc82046126ff10e2c1cb4ba9c5bb1
csorndawg <zaccsorny@yahoo.com>
2025-04-21 06:24:04 -0400

    Update swenv default VENV path. Add symlinks from existing python VENV dirs to dirpath given as SWENV default

--- nvim.d/lua/plugins/swenv.nvim.lua
+++ nvim.d/lua/plugins/swenv.nvim.lua
@@ -4,6 +4,6 @@ return {
 		post_set_venv = function(venv_path)
 			vim.g.python3_host_prog = venv_path .. "/bin/python"
 		end,
-		venv_dirs = { "~/.local/state/uv/uvv" }, -- Adjust this based on where uv stores environments
+		venv_dirs = { "~/.local/state/python/uv/uvv" }, -- Adjust this based on where uv stores environments
 	},
 }

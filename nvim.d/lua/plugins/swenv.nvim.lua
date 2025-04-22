26a2b2e7c5e6854f1ac7f64d248fddef19a8c567
csorndawg <zaccsorny@yahoo.com>
2025-04-21 22:50:16 -0400

    Add snacks plugin, but only install specified snacks submodules. Setup configuration files for snacks submodules and their keymaps.

--- nvim.d/lua/plugins/swenv.nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"AckslD/swenv.nvim",
-	opts = {
-		post_set_venv = function(venv_path)
-			vim.g.python3_host_prog = venv_path .. "/bin/python"
-		end,
-		venv_dirs = { "~/.local/state/python/uv/uvv" }, -- Adjust this based on where uv stores environments
-	},
-}

4cacaf62edc40076d9d5e52eee98d3bd9fe92b33
csorndawg <zaccsorny@yahoo.com>
2025-04-23 00:45:15 -0400

    Remove unused nvim.d/ files. Update alarcritty config

--- nvim.d/lua/extra/cmp-test.lua
+++
@@ -1,8 +0,0 @@
--- @TODO: See if this can be refactored into a `cmp` extra module, where
--- 				all other cmp configuration exist
-require("luasnip.loaders.from_vscode").lazy_load({
-	paths = {
-		-- allow luansip to access community snippets
-		"$HOME/.local/share/nvim/lazy/friendly-snippets/",
-	},
-})

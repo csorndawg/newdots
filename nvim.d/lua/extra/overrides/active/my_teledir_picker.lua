5d72febc049f9a760479a78609a8dc1136b8e4d8
csorndawg <zaccsorny@yahoo.com>
2025-04-29 19:55:59 -0400

    Add custom lua module, for a personal swenv-like venv switcher. Removed stale (now handled by the custom module) keymaps from swenv_extra.

---
+++ nvim.d/lua/extra/overrides/active/my_teledir_picker.lua
@@ -0,0 +1,133 @@
+-- lua/extra/my_directory_picker.lua
+local M = {}
+
+local Path = require("plenary.path")
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local scan = require("plenary.scandir")
+
+-- You can change this to any root you'd like
+local predefined_root = vim.fn.expand("~/.local/state/python/uv")
+
+-- Stores the selected directory (can be accessed externally)
+M.selected_directory = nil
+
+local function get_dirs(path, opts)
+	return scan.scan_dir(
+		path,
+		vim.tbl_deep_extend("force", {
+			only_dirs = true,
+			silent = true,
+		}, opts or {})
+	)
+end
+
+-- also check for any Venvs in CWD filetree, and add any to Telescope picker selection list
+local function get_valid_venv_dirs(base_path)
+	local all_dirs = scan.scan_dir(base_path, {
+		only_dirs = true,
+		hidden = true,
+		depth = nil,
+	})
+
+	local function has_pyvenv_cfg(dir)
+		return vim.fn.filereadable(Path:new(dir, "pyvenv.cfg"):absolute()) == 1
+	end
+
+	local valid_dirs = {}
+	for _, dir in ipairs(all_dirs) do
+		if has_pyvenv_cfg(dir) then
+			table.insert(valid_dirs, vim.fn.fnamemodify(dir, ":p"))
+		end
+	end
+
+	return valid_dirs
+end
+
+-- functions for fetching/activating venv from Telescope picker selection
+local function find_activate_script(base_dir)
+	local matches = scan.scan_dir(base_dir, {
+		depth = nil,
+		search_pattern = "bin/activate$",
+		add_dirs = false,
+		hidden = true,
+	})
+	return matches[1]
+end
+
+local function launch_tmux_split(activate_path)
+	local cmd =
+		string.format([[tmux split-window -h "bash -c 'source %s; exec $SHELL'"]], vim.fn.fnameescape(activate_path))
+	vim.fn.system(cmd)
+end
+
+function M.pick_directory()
+	local results_set = {}
+
+	-- Add top-level project dirs
+	for _, dir in ipairs(get_dirs(predefined_root, { depth = 1 })) do
+		results_set[vim.fn.fnamemodify(dir, ":p")] = true
+	end
+
+	-- Add filtered CWD subdirs with pyvenv.cfg
+	for _, dir in ipairs(get_valid_venv_dirs(vim.fn.getcwd())) do
+		results_set[dir] = true
+	end
+
+	local results = vim.tbl_keys(results_set)
+	table.sort(results)
+
+	pickers
+		.new({}, {
+			prompt_title = "Select a Directory",
+			finder = finders.new_table({ results = results }),
+			sorter = conf.generic_sorter({}),
+
+			attach_mappings = function(prompt_bufnr, _)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+					local selection = action_state.get_selected_entry()
+					if not selection then
+						return
+					end
+
+					local venv_path = Path:new(selection[1])
+					local activate_path = venv_path:joinpath("bin", "activate"):absolute()
+
+					if vim.fn.filereadable(activate_path) ~= 1 then
+						vim.notify("No 'bin/activate' found in " .. activate_path, vim.log.levels.WARN)
+						return
+					end
+
+					-- Get current buffer path
+					local buf_path = vim.api.nvim_buf_get_name(0)
+					local is_abs = vim.fn.fnamemodify(buf_path, ":p") == buf_path and buf_path ~= ""
+
+					-- Construct tmux command
+					local tmux_cmd = is_abs
+							and string.format(
+								[[tmux split-window -h 'source "%s" && nvim "%s"']],
+								activate_path,
+								buf_path
+							)
+						or string.format([[tmux split-window -h 'source "%s"']], activate_path)
+
+					vim.fn.system(tmux_cmd)
+				end)
+
+				return true
+			end,
+		})
+		:find()
+end
+
+-- define keymaps that reference logic defined above
+vim.keymap.set("n", "<leader>cvp", function()
+	M.pick_directory()
+end, { desc = "Pick venv to activate in Tmux split" })
+
+-- return module with function and keymaps
+return M
--- nvim.d/lua/extra/overrides/active/swenv_extra.lua
+++ nvim.d/lua/extra/overrides/active/swenv_extra.lua
@@ -1,80 +1,11 @@
--- experimental/test config for swenv
-
--- @FIXME:
--- @FIXME:
--- vim.keymap.set(
--- 	"n",
--- 	"<Leader>cva",
--- 	<cmd>lua require('swenv.api').auto_venv()<cr>,
--- 	{ desc = "Automake venv", noremap = true }
--- )
-
--- @TESTME: Testing "vim.notify" integration
-vim.keymap.set(
-	"n",
-	"<Leader>cvp",
-	-- <cmd>lua require('swenv.api').pick_venv()<cr>,
-	function()
-		local ok, err = pcall(require("swenv.api").pick_venv)
-		if not ok then
-			vim.notify("swenv pick venv error: " .. err, vim.log.levels.ERROR)
-		end
-	end,
-	{ desc = "Pick venv", noremap = true }
-)
-
--- -- @FIXME:
--- vim.keymap.set(
--- 	"n",
--- 	"<Leader>cvs",
--- 	<cmd>lua require('swenv.api').get_current_venv()<cr>,
+-- Unable to get any other Swenv functions, beside the one used in the below keymap, to work
+-- Created a custom module ("my_teledir_picker") to replicate the functionality for the
+-- broken functions like pick_venv().
 --
--- 	{ desc = "Search/Set venv", noremap = true }
--- )
---
+-- Even though its just a single keymap/function DO NOT DELETE THIS FUNCTION.
+-- It still is useful for my workflow, and is used alongside my custom module.
 
--- @TEST: vim.notify to display "set_venv" results
 vim.keymap.set("n", "<Leader>cvv", function()
 	local venv = require("swenv.api").set_venv()
 	vim.notify("Current venv: " .. (venv or "No active venv"), vim.log.levels.INFO)
 end, { desc = "View current venv", noremap = true })
-
--- @NOTE: Keymap copnfiguration not loaded when "local M" module present. Removing and retesting.
--- local M = {}
-
--- local SWENV = require("swenv.api")
---
--- -- automate new venv directory creation and activation
--- -- @NOTE: Commenting out since I only want autovenv to run ocassionally. Sometimes I will want to reuse
--- --        existing venv, and other times I want a fresh one for a fresh project.
--- vim.api.nvim_create_autocmd("FileType", {
--- 	pattern = { "python" },
--- 	callback = function()
--- 		SWENV.auto_venv()
--- 	end,
--- })
---
--- NOTE: Move all VENV code actions to 'V' subgroup label (<leader>cv). Can also
---        move to "Python" subgroup label if not enough actions to warrant 'V'.
-
--- @TODO: Load 'lualine' and add SWENV info to lualine section
--- swenv docs copypasta
-
--- --  add to lualine component
--- sections = {
---         ...
---         lualine_a = 'swenv' -- uses default options
---         lualine_x = { 'swenv', icon = '<icon>' } -- passing lualine component options
---         ...
---     }
---
--- -- only show if FT=python
---     {
---         "swenv",
---         cond = function()
---             return vim.bo.filetype == "python"
---
---         end,
---     }
-
--- return M

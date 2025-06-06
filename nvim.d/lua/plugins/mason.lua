77cb6f46ba3bb98100f15fb5f2b9cb7a7fb0d03b
csorndawg <zaccsorny@yahoo.com>
2025-06-06 01:26:40 -0400

    TESTING code - trying to debug MASON EXTRA ansiblels error in messages

--- nvim.d/lua/plugins/mason.lua
+++ nvim.d/lua/plugins/mason.lua
@@ -26,37 +26,36 @@
 --
 
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
 
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    event = "VeryLazy",
-    config = function()
-      local mason_tools = require("extra.mason.mason")
-      local all_tools = {}
-      vim.list_extend(all_tools, mason_tools.lsp)
-      vim.list_extend(all_tools, mason_tools.null_ls)
-      vim.list_extend(all_tools, mason_tools.dap)
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		-- event = "VeryLazy",
+		config = function()
+			local mason_tools = require("extra.mason.mason")
+			local all_tools = {}
+			vim.list_extend(all_tools, mason_tools.lsp)
+			vim.list_extend(all_tools, mason_tools.null_ls)
+			vim.list_extend(all_tools, mason_tools.dap)
 
-      require("mason-tool-installer").setup({
-        ensure_installed = all_tools,
-        auto_update = false,
-        run_on_start = true,
-        start_delay = 3000, -- ms
-        debounce_hours = 5,
-      })
-    end,
-  },
+			require("mason-tool-installer").setup({
+				ensure_installed = all_tools,
+				auto_update = false,
+				run_on_start = true,
+				start_delay = 3000, -- ms
+				debounce_hours = 5,
+			})
+		end,
+	},
 }
-

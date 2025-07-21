3a70b144d35922f23cdc312766b74f4c4b1584c9
csorndawg <zaccsorny@yahoo.com>
2025-07-20 20:53:40 -0400

    Add scroll up/down keymaps for whichkey popup menu

--- nvim.d/lua/plugins/which-key.lua
+++ nvim.d/lua/plugins/which-key.lua
@@ -7,6 +7,10 @@ return {
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
+			popup_mappings = {
+				scroll_down = "<C-d>",
+				scroll_up = "<C-u>",
+			},
 		},
 		keys = {
 			{
